8e2ccee86021e2848b056e1419e26bbe9267dc3c
Joe Carter <joe@simpleweb.co.uk>
2020-01-06 13:31:33 +0000

    Improve usability
    - Make npm install also install deps of subpackages
    - Add a global setup to check that the broker is available
    - Make the broker logging more useful + less confusing

--- packages/openactive-broker-microservice/app.js
+++ packages/openactive-broker-microservice/app.js
@@ -9,12 +9,15 @@ process.env["PORT"] = 3000;
 
 var app = express();
 
-app.use(logger("dev"));
+if (REQUEST_LOGGING_ENABLED) {
+  app.use(logger("dev"));
+}
 app.use(express.json());
 
 var PORT = 3000;
 var BOOKING_API_BASE = config.get("microservice.bookingApiBase");
 var FEED_BASE = config.get("microservice.openFeedBase");
+var REQUEST_LOGGING_ENABLED = config.get("requestLogging");
 
 nSQL().createDatabase({
   tables: [
@@ -129,11 +132,10 @@ app.get("/feeds/scheduled-sessions", function(req, res) {
     .orderBy({ feedModified: "asc", jsonLdId: "asc" })
     .limit(PAGE_SIZE)
     /*.join({
-    type: "left",
-    with: {table: "SessionSeries"},
-    on: ["jsonLdParentId","=","SessionSeries.jsonLdId"]
-  })*/
-    .exec()
+  type: "left",
+  with: {table: "SessionSeries"},
+  on: ["jsonLdParentId","=","SessionSeries.jsonLdId"]
+})*/ .exec()
     .then(rows => {
       if (rows.length > 0) {
         var aggregatedRows = [];
@@ -181,10 +183,16 @@ var responses = {
   /* Keyed by expression =*/
 };
 
+app.get("/health-check", function(req, res) {
+  res.send("openactive-broker");
+});
+
 app.get("/get-match/:expression", function(req, res) {
   // respond with json
   if (req.params.expression) {
     var expression = req.params.expression;
+
+    console.log("listening for " + expression);
 
     // Stash the response and reply later when an event comes through (kill any existing expression still waiting)
     if (responses[expression] && responses[expression] !== null)
@@ -248,11 +256,13 @@ getRPDE(BOOKING_API_BASE + "orders-rpde", monitorOrdersPage);
 sessionSeriesMap = {};
 
 function ingestSessionSeriesPage(rpde, pageNumber) {
-  console.log(
-    `RPDE kind: SessionSeries, page: ${pageNumber + 1 || 0}, length: ${
-      rpde.items.length
-    }, next: '${rpde.next}'`
-  );
+  if (REQUEST_LOGGING_ENABLED) {
+    console.log(
+      `RPDE kind: SessionSeries, page: ${pageNumber + 1 || 0}, length: ${
+        rpde.items.length
+      }, next: '${rpde.next}'`
+    );
+  }
 
   var newItems = rpde.items.map(item => ({
     id: item.id,
@@ -301,11 +311,13 @@ function ingestSessionSeriesPage(rpde, pageNumber) {
 }
 
 function ingestScheduledSessionPage(rpde, pageNumber) {
-  console.log(
-    `RPDE kind: ScheduledSession, page: ${pageNumber + 1 || 0}, length: ${
-      rpde.items.length
-    }, next: '${rpde.next}'`
-  );
+  if (REQUEST_LOGGING_ENABLED) {
+    console.log(
+      `RPDE kind: ScheduledSession, page: ${pageNumber + 1 || 0}, length: ${
+        rpde.items.length
+      }, next: '${rpde.next}'`
+    );
+  }
 
   nSQL("ScheduledSessions")
     .query(
@@ -333,18 +345,22 @@ function ingestScheduledSessionPage(rpde, pageNumber) {
 }
 
 function monitorPage(rpde, pageNumber) {
-  console.log(
-    `RPDE kind: Opportunity Monitoring, length: ${rpde.items.length}, next: '${rpde.next}'`
-  );
+  if (REQUEST_LOGGING_ENABLED) {
+    console.log(
+      `RPDE kind: Opportunity Monitoring, length: ${rpde.items.length}, next: '${rpde.next}'`
+    );
+  }
 
   rpde.items.forEach(item => {
     // TODO: make this regex loop (note ignore deleted items)
-    if (
-      item.data &&
-      item.data.superEvent &&
-      responses[item.data.superEvent.name]
-    ) {
-      responses[item.data.superEvent.name].send(item);
+    if (item.data && item.data.superEvent) {
+      if (responses[item.data.superEvent.name]) {
+        responses[item.data.superEvent.name].send(item);
+
+        console.log("seen and dispatched " + item.data.superEvent.name);
+      } else {
+        console.log("saw " + item.data.superEvent.name);
+      }
     }
   });
 
@@ -352,9 +368,11 @@ function monitorPage(rpde, pageNumber) {
 }
 
 function monitorOrdersPage(rpde, pageNumber) {
-  console.log(
-    `RPDE kind: Orders Monitoring, length: ${rpde.items.length}, next: '${rpde.next}'`
-  );
+  if (REQUEST_LOGGING_ENABLED) {
+    console.log(
+      `RPDE kind: Orders Monitoring, length: ${rpde.items.length}, next: '${rpde.next}'`
+    );
+  }
 
   rpde.items.forEach(item => {
     // TODO: make this regex loop (note ignore deleted items)
---
+++ packages/openactive-integration-tests/test/global-setup.js
@@ -0,0 +1,25 @@
+const assert = require("assert");
+const axios = require("axios");
+const config = require("config");
+
+var BOOKING_API_BASE = config.get("tests.bookingApiBase");
+var MICROSERVICE_BASE = config.get("tests.microserviceApiBase");
+
+async function ping() {
+  let response = await axios.get(MICROSERVICE_BASE + "health-check/");
+
+  assert.strictEqual(response.data, "openactive-broker2");
+
+  return true;
+}
+
+module.exports = async () => {
+  try {
+    await ping();
+  } catch (error) {
+    console.error(
+      "The broker microservice is unreachable. This is a pre-requisite for the test suite."
+    );
+    throw error;
+  }
+};
