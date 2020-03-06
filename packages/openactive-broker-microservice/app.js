c6cb20be17a8b1b23b9558d0cf6abeafde63a4cf
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2020-03-06 09:29:35 +0000

    Significant optimisations to microservice

--- packages/openactive-broker-microservice/app.js
+++ packages/openactive-broker-microservice/app.js
@@ -21,53 +21,13 @@ if (REQUEST_LOGGING_ENABLED) {
 app.use(express.json());
 
 // nSQL joins appear to be slow, even with indexes. This is an optimisation pending further investigation
-parentOpportunityMap = {};
-parentOpportunityRpdeMap = {};
-opportunityMap = {};
-opportunityRpdeMap = {};
+parentOpportunityMap = new Map();
+parentOpportunityRpdeMap = new Map();
+opportunityMap = new Map();
+opportunityRpdeMap = new Map();
+rowStoreMap = new Map();
+parentIdIndex = new Map();
 
-nSQL().createDatabase({
-  tables: [
-    {
-      name: "ParentOpportunity",
-      model: {
-        "id:int": { pk: true },
-        "modified:int": {},
-        "deleted:bool": {},
-        "jsonLdId:string": {},
-        "feedModified:int": {},
-        "jsonLd:obj": {}
-      },
-      indexes: {
-        "feedModified:int": {},
-        "jsonLdId:string": {}
-      }
-    },
-    {
-      name: "Opportunity",
-      model: {
-        "id:int": { pk: true },
-        "modified:int": {},
-        "deleted:bool": {},
-        "jsonLdId:string": {},
-        "feedModified:int": {},
-        "jsonLdType:string": {},
-        "jsonLd:obj": {},
-        "jsonLdParentId:string": {},
-        "parentIngested:bool": {}
-      },
-      indexes: {
-        "jsonLdParentId:string": {
-          /*  foreignKey: { // foreign key property
-                      target: "ParentOpportunity.jsonLdId" // parent Table.column or Table.nested.column
-                  }*/
-        },
-        "feedModified:int": {},
-        "jsonLdId:string": {}
-      }
-    }
-  ]
-});
 
 function getRPDE(url, cb) {
   var headers = {
@@ -93,14 +53,17 @@ function getRPDE(url, cb) {
         throw `Base URL of RPDE 'next' property ("${getBaseUrl(json.next)}") does not match base URL of RPDE page ("${url}")`;
       }
 
-
       cb(json);
+    } else if (!response) {
+      console.log(`Error for RPDE feed "${url}": ${error}. Response: ${body}`);
+      // Force retry, after a delay
+      setTimeout(x => getRPDE(url, cb), 5000);
     } else if (response.statusCode === 404) {
       console.log(`Not Found error for RPDE feed "${url}", feed will be ignored: ${error}.`);
       // Stop polling feed
     } else {
       console.log(`Error ${response.statusCode} for RPDE page "${url}": ${error}. Response: ${body}`);
-      // Fake next page to force retry, after a delay
+      // Force retry, after a delay
       setTimeout(x => getRPDE(url, cb), 5000);
     }
   });
@@ -135,114 +98,18 @@ function getRandomBookableOpportunity(type) {
 }
 
 function getOpportunityById(opportunityId) {
-  if (opportunityMap[opportunityId] && parentOpportunityMap[opportunityMap[opportunityId].superEvent || opportunityMap[opportunityId].facilityUse]) {
+  var opportunity = opportunityMap.get(opportunityId);
+  if (opportunity && parentOpportunityMap.has(opportunity.superEvent || opportunity.facilityUse)) {
     return Object.assign(
       {},
-      opportunityMap[opportunityId],
+      opportunity,
       { 
-        superEvent: parentOpportunityMap[opportunityMap[opportunityId].superEvent],
-        facilityUse: parentOpportunityMap[opportunityMap[opportunityId].facilityUse]
+        superEvent: parentOpportunityMap.get(opportunity.superEvent),
+        facilityUse: parentOpportunityMap.get(opportunity.facilityUse)
       }
     )
   }
 }
-
-app.get("/feeds/opportunities", function(req, res) {
-  var afterId = null;
-  var afterTimestamp = null;
-  if (req.query && req.query.afterId && req.query.afterTimestamp) {
-    afterId = req.query.afterId;
-    afterTimestamp = parseInt(req.query.afterTimestamp);
-  }
-
-  var baseUrl = "http://localhost:" + PORT + "/feeds/opportunities";
-  var PAGE_SIZE = 500;
-
-  nSQL("Opportunity")
-    .query("select", [
-      "jsonLdId",
-      "deleted",
-      "feedModified",
-      "jsonLd",
-      "jsonLdParentId",
-      "jsonLdType"
-      // "ParentOpportunity.jsonLd",
-      //"ParentOpportunity.jsonLdId",
-    ])
-    .where(
-      afterTimestamp != null
-        ?
-        [ 
-          ["parentIngested", "=", true],
-          "AND",
-          [
-            [
-              [
-                ["feedModified", "=", afterTimestamp],
-                "AND",
-                ["jsonLdId", ">", afterId]
-              ],
-              "OR",
-              ["feedModified", ">", afterTimestamp]
-            ],
-            "AND",
-            ["feedModified", "<=", Date.now()] // Only show items that have been updated and are ready for display
-          ]
-        ]
-        :
-        ["parentIngested", "=", true]
-    )
-    .orderBy({ feedModified: "asc", jsonLdId: "asc" })
-    .limit(PAGE_SIZE)
-    /*.join({
-  type: "left",
-  with: {table: "ParentOpportunity"},
-  on: ["jsonLdParentId","=","ParentOpportunity.jsonLdId"]
-})*/ .exec()
-    .then(rows => {
-      if (rows.length > 0) {
-        var aggregatedRows = [];
-        var lastId = rows[rows.length - 1]["jsonLdId"];
-        var lastTimestamp = parseInt(rows[rows.length - 1]["feedModified"]);
-        res.json({
-          next:
-            baseUrl +
-            "?afterTimestamp=" +
-            lastTimestamp +
-            "&afterId=" +
-            encodeURIComponent(lastId),
-          items: rows.map(row => ({
-            state: row["deleted"] ? "deleted" : "updated",
-            id: row["jsonLdId"],
-            modified: row["feedModified"],
-            data: Object.assign(
-              {},
-              row["jsonLd"],
-              row["jsonLdType"] == "slot" ? // Note parentOpportunityMap used as much faster than nSQL join
-              { facilityUse: parentOpportunityMap[row["jsonLdParentId"]] } :
-              { superEvent: parentOpportunityMap[row["jsonLdParentId"]] }
-            )
-          })),
-          license: "https://creativecommons.org/licenses/by/4.0/"
-        });
-        res.end();
-      } else {
-        res.json({
-          next:
-            baseUrl +
-            (afterTimestamp == null
-              ? ""
-              : "?afterTimestamp=" +
-                afterTimestamp +
-                "&afterId=" +
-                encodeURIComponent(afterId)),
-          items: [],
-          license: "https://creativecommons.org/licenses/by/4.0/"
-        });
-        res.end();
-      }
-    });
-});
 
 var responses = {
   /* Keyed by expression =*/
@@ -356,7 +223,7 @@ getRPDE(FEED_BASE + "headline-events", ingestOpportunityPage);
 getRPDE(FEED_BASE + "courses", ingestOpportunityPage);
 
 // Start monitoring first page of internal feed
-getRPDE("http://localhost:" + PORT + "/feeds/opportunities", monitorPage);
+//getRPDE("http://localhost:" + PORT + "/feeds/opportunities", monitorPage);
 
 // Only poll orders feed if enabled
 if (BOOKING_API_BASE != null)
@@ -373,55 +240,30 @@ function ingestParentOpportunityPage(rpde, pageNumber) {
     );
   }
 
-  var newItems = rpde.items.map(item => ({
-    id: item.id,
-    modified: item.modified,
-    deleted: item.state == "deleted",
-    feedModified: Date.now() + 1000, // 1 second in the future
-    jsonLdId: item.state == "deleted" ? null : item.data['@id'] || item.data['id'],
-    jsonLd: item.state == "deleted" ? null : item.data
-  }));
-
   rpde.items.forEach(item => {
     if (item.state == "deleted") {
-      var jsonLdId = parentOpportunityRpdeMap[item.id];
-      delete parentOpportunityMap[jsonLdId];
-      delete parentOpportunityRpdeMap[item.id];
+      var jsonLdId = parentOpportunityRpdeMap.get(item.id);
+      parentOpportunityMap.delete(jsonLdId);
+      parentOpportunityRpdeMap.delete(item.id);
     } else {
       var jsonLdId = item.data['@id'] || item.data['id'];
-      parentOpportunityRpdeMap[item.id] = jsonLdId;
-      parentOpportunityMap[jsonLdId] = item.data;
+      parentOpportunityRpdeMap.set(item.id, jsonLdId);
+      parentOpportunityMap.set(jsonLdId, item.data);
     }
   });
 
-  nSQL("ParentOpportunity")
-    .query("upsert", newItems)
-    .exec()
-    .then(() => {
-      // Update any children that reference this parent
-      nSQL("Opportunity")
-        .query("upsert", {
-          feedModified: Date.now() + 1000, // 1 second in the future
-          parentIngested: true
-        })
-        .where([
-          "jsonLdParentId",
-          "IN",
-          rpde.items
-            .filter(item => item.state != "deleted")
-            .map(item => item.data['@id'] || item.data['id'])
-        ])
-        .exec()
-        .then(() => {
-          setTimeout(
-            x =>
-              getRPDE(rpde.next, x =>
-                ingestParentOpportunityPage(x, pageNumber + 1 || 0)
-              ),
-            200
-          );
-        });
-    });
+  // As these parent opportunities have been updated, update all child items for these parent IDs
+  touchOpportunityItems(rpde.items
+    .filter(item => item.state != "deleted")
+    .map(item => item.data['@id'] || item.data['id']));
+
+  setTimeout(
+    x =>
+      getRPDE(rpde.next, x =>
+        ingestParentOpportunityPage(x, pageNumber + 1 || 0)
+      ),
+    200
+  );
 }
 
 function ingestOpportunityPage(rpde, pageNumber) {
@@ -436,58 +278,111 @@ function ingestOpportunityPage(rpde, pageNumber) {
 
   rpde.items.forEach(item => {
     if (item.state == "deleted") {
-      var jsonLdId = opportunityRpdeMap[item.id];
-      delete opportunityMap[jsonLdId];
-      delete opportunityRpdeMap[item.id];
+      var jsonLdId = opportunityRpdeMap.get(item.id);
+      opportunityMap.delete(jsonLdId);
+      opportunityRpdeMap.delete(item.id);
+
+      deleteOpportunityItem(jsonLdId);
     } else {
       var jsonLdId = item.data['@id'] || item.data['id'];
-      opportunityRpdeMap[item.id] = jsonLdId;
-      opportunityMap[jsonLdId] = item.data;
+      opportunityRpdeMap.set(item.id, jsonLdId);
+      opportunityMap.set(jsonLdId, item.data);
+
+      storeOpportunityItem(item);
     }
   });
 
-  nSQL("Opportunity")
-    .query(
-      "upsert",
-      rpde.items.map(item => ({
-        id: item.id,
-        modified: item.modified,
-        deleted: item.state == "deleted",
-        feedModified: Date.now() + 1000, // 1 second in the future,
-        jsonLdId: item.state == "deleted" ? null : item.data['@id'] || item.data['id'],
-        jsonLd: item.state == "deleted" ? null : item.data,
-        jsonLdType: item.state == "deleted" ? null : item.data['@type'] || item.data['type'],
-        jsonLdParentId: item.state == "deleted" ? null : item.data.superEvent || item.data.facilityUse,
-        parentIngested: item.state == "deleted" ? false : typeof parentOpportunityMap[item.data.superEvent] !== "undefined" || typeof parentOpportunityMap[item.data.facilityUse] !== "undefined"
-      }))
-    )
-    .exec()
-    .then(() => {
-      setTimeout(
-        x =>
-          getRPDE(rpde.next, x =>
-            ingestOpportunityPage(x, pageNumber + 1 || 0)
-          ),
-        200
-      );
-    });
+  setTimeout(
+    x =>
+      getRPDE(rpde.next, x =>
+        ingestOpportunityPage(x, pageNumber + 1 || 0)
+      ),
+    200
+  );
 }
 
-function monitorPage(rpde, pageNumber) {
-  if (REQUEST_LOGGING_ENABLED) {
-    console.log(
-      `RPDE kind: Opportunity Monitoring, length: ${rpde.items.length}, next: '${rpde.next}'`
-    );
+function touchOpportunityItems(parentIds) {
+  var opportunitiesToUpdate = new Set();
+
+  parentIds.forEach(parentId => {
+    if (parentIdIndex.has(parentId)) {
+      parentIdIndex.get(parentId).forEach(jsonLdId => {
+        opportunitiesToUpdate.add(jsonLdId);
+      });
+    }
+  })
+
+  opportunitiesToUpdate.forEach(jsonLdId => {
+    if (rowStoreMap.has(jsonLdId)) {
+      var row = rowStoreMap.get(jsonLdId);
+      row.feedModified = Date.now() + 1000; // 1 second in the future
+      row.parentIngested = true;
+      processRow(row);
+    }
+  });
+}
+
+function deleteOpportunityItem(jsonLdId) {
+  var row = rowStoreMap.get(jsonLdId);
+  if (row) {
+    var idx = parentIdIndex.get(row.jsonLdParentId);
+    if (idx) {
+      idx.delete(jsonLdId);
+    }
+    rowStoreMap.delete(jsonLdId);
+  }
+}
+
+function storeOpportunityItem(item) {
+  var row = {
+    id: item.id,
+    modified: item.modified,
+    deleted: item.state == "deleted",
+    feedModified: Date.now() + 1000, // 1 second in the future,
+    jsonLdId: item.state == "deleted" ? null : item.data['@id'] || item.data['id'],
+    jsonLd: item.state == "deleted" ? null : item.data,
+    jsonLdType: item.state == "deleted" ? null : item.data['@type'] || item.data['type'],
+    jsonLdParentId: item.state == "deleted" ? null : item.data.superEvent || item.data.facilityUse,
+    parentIngested: item.state == "deleted" ? false : parentOpportunityMap.has(item.data.superEvent) || parentOpportunityMap.has(item.data.facilityUse)
+  };
+
+  if (row.jsonLdParentId != null && row.jsonLdId != null) {
+    if (!parentIdIndex.has(row.jsonLdParentId)) parentIdIndex.set(row.jsonLdParentId, new Set());
+    parentIdIndex.get(row.jsonLdParentId).add(item.jsonLdId);
   }
 
-  rpde.items.forEach(item => {
+  rowStoreMap.set(row.jsonLdId, row);
+
+  if (row.parentIngested) {
+    processRow(row);
+  }
+}
+
+function processRow(row) {
+  var newItem = {
+    state: row["deleted"] ? "deleted" : "updated",
+    id: row["jsonLdId"],
+    modified: row["feedModified"],
+    data: Object.assign(
+      {},
+      row["jsonLd"],
+      row["jsonLdType"] == "Slot" ?
+      { facilityUse: parentOpportunityMap.get(row["jsonLdParentId"]) } :
+      { superEvent: parentOpportunityMap.get(row["jsonLdParentId"]) }
+    )
+  };
+
+  processOpportunityItem(newItem);
+}
+
+function processOpportunityItem(item) {
     if (item.data) {
       var id = item.data['@id'] || item.data['id'];
       var type = item.data['@type'] || item.data['type']; 
 
       // Check for bookability
       var startDate = item.data.startDate;
-      var offers = item.data.offers || item.data.superEvent.offers; // Note FacilityUse does not have bookable offers, as it does not allow inheritance
+      var offers = item.data.offers || (item.data.superEvent && item.data.superEvent.offers) || (item.data.facilityUse && item.data.facilityUse.offers); // Note FacilityUse does not have bookable offers, as it does not allow inheritance
       var remainingCapacity = item.data.remainingAttendeeCapacity || item.data.remainingUses;
       var eventStatus = item.data.eventStatus;
 
@@ -552,9 +447,6 @@ function monitorPage(rpde, pageNumber) {
         console.log(`saw ${isBookable ? "bookable " : ""}${id}${bookableIssueList}`);
       }
     }
-  });
-
-  setTimeout(x => getRPDE(rpde.next, monitorPage), 200);
 }
 
 function monitorOrdersPage(rpde, pageNumber) {
