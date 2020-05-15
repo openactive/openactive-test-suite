992c5c846591f5e079171a4b2fcc6c9b5e8b6c0a
Joe Carter <theupperquartile@gmail.com>
2020-05-15 11:22:48 +0100

    Add missing file

---
+++ packages/openactive-integration-tests/test/test-framework/jasmine-state-reporter.js
@@ -0,0 +1,69 @@
+const EventEmitter = require('events');
+
+class JasmineStateReporter {
+  constructor() {
+    this.emitter = new EventEmitter();
+
+    this.suiteStarted = this.suiteStarted.bind(this);
+    this.suiteDone = this.suiteDone.bind(this);
+    this.specStarted = this.specStarted.bind(this);
+    this.specDone = this.specDone.bind(this);
+  }
+
+  suiteStarted(suite) {
+    if (this.currentSuite) {
+      suite._parent = this.currentSuite;
+    }
+    this.currentSuite = suite;
+
+    this.emitter.emit('suite-started', suite);
+  };
+
+  suiteDone(suite) {
+    if (this.currentSuite) {
+      this.currentSuite = this.currentSuite._parent;
+    }
+
+    this.emitter.emit('suite-done', suite);
+  };
+
+  specStarted(result) {
+    this.currentTest = result;
+
+    this.emitter.emit('spec-started', result);
+  };
+
+  specDone(result) {
+    this.currentTest = result;
+
+    this.emitter.emit('spec-done', result);
+  };
+
+  get fullName() {
+    if (this.currentTest) {
+      return this.currentTest.fullName;
+    }
+    else if (this.currentSuite) {
+      return this.currentSuite.fullName;
+    }
+  }
+
+  get ancestorTitles() {
+    let suite = this.currentSuite;
+    let path = [];
+    while (suite) {
+      path.unshift(suite.description);
+
+      suite = suite._parent;
+    }
+    return path;
+  }
+
+  on(...args) {
+    return this.emitter.on(...args);
+  }
+}
+
+module.exports = {
+  JasmineStateReporter
+};
