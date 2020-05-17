26f842521fe83c4f1edfe181494f5309d95a90d5
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2020-05-17 22:41:57 +0100

    Use accessService.endpointURL from dataset site

---
+++ packages/openactive-integration-tests/test/testEnvironment.js
@@ -0,0 +1,34 @@
+const NodeEnvironment = require('jest-environment-node');
+const axios = require("axios");
+const config = require("config");
+
+const MICROSERVICE_BASE = config.get("tests.microserviceApiBase");
+const TEST_DATASET_IDENTIFIER = config.get("tests.testDatasetIdentifier");
+
+class TestEnvironment extends NodeEnvironment {
+  constructor(config) {
+    super(config);
+  }
+
+  async setup() {
+    await super.setup();
+
+    // Get endpoint URL from Dataset Site
+    let response = await axios.get(MICROSERVICE_BASE + "dataset-site");
+    if (response && response.data && response.data.accessService && response.data.accessService.endpointURL) {
+      this.global.BOOKING_API_BASE = response.data.accessService.endpointURL;
+    }
+    this.global.MICROSERVICE_BASE = MICROSERVICE_BASE;
+    this.global.TEST_DATASET_IDENTIFIER = TEST_DATASET_IDENTIFIER;
+  }
+
+  async teardown() {
+    await super.teardown();
+  }
+
+  runScript(script) {
+    return super.runScript(script);
+  }
+}
+
+module.exports = TestEnvironment;
