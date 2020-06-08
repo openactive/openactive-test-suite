694db8a74ef5929c6cc3755c75aefa977557363f
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2020-06-08 14:57:04 +0100

    Fix CI attempt

--- publish-example-output.sh
+++ publish-example-output.sh
@@ -22,10 +22,12 @@ npm install --prefix packages/openactive-broker-microservice
 npm install --prefix packages/openactive-integration-tests
 
 # Start broker microservice in the background
+echo 'Starting broker microservice...'
 npm start --prefix packages/openactive-broker-microservice &
 pid=$!
 
 # Run tests in random mode
+echo 'Running integration tests...'
 rm -rf ./packages/openactive-integration-tests/output/
 NODE_CONFIG='{"useRandomOpportunities": true}' npm test --prefix packages/openactive-integration-tests --runInBand -- test/features/
 cp ./packages/openactive-integration-tests/output/* ./publish/random/
@@ -36,12 +38,14 @@ cp ./packages/openactive-integration-tests/output/* ./publish/random/
 # cp ./packages/openactive-integration-tests/output/* ./publish/controlled/
 
 # Kill broker microservice
+echo 'Killing broker microservice...'
 kill $pid
 
 cd publish
 
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
+echo 'Committing files to Git...'
 git add .
 git commit -m "Deploy to GitHub Pages - Static"
 
@@ -50,6 +54,7 @@ git commit -m "Deploy to GitHub Pages - Static"
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
 # FIXME should be authorised via key
+echo 'Pushing to gh-pages...'
 test $TRAVIS_BRANCH = "master" && git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
 
 cd ..
