9596e7be3b96387089705280bebb8ada4cfa576b
Luke Winship <luke.winship@gmail.com>
2020-08-03 17:22:51 +0100

    create attemptC1WithNewState()

--- packages/openactive-integration-tests/test/features/core/amending-order-quote/implemented/amend-c1-test.js
+++ packages/openactive-integration-tests/test/features/core/amending-order-quote/implemented/amend-c1-test.js
@@ -5,6 +5,7 @@ const { B, C1, Common, GetMatch } = require('../../../../shared-behaviours');
 const { FlowHelper } = require('../../../../helpers/flow-helper');
 const { RequestState } = require('../../../../helpers/request-state');
 const { generateUuid } = require('../../../../helpers/generate-uuid');
+const { C2 } = require('../../../../shared-behaviours/c2');
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'core',
@@ -39,59 +40,61 @@ FeatureHelper.describeFeature(module, {
         });
       });
   };
+
+  /**
+   * Create a new state and flow helper (to represent a distinct batch of opportunities)
+   * and then fetch some opportunities and run C1
+   *
+   * Note: This generates jest blocks like `beforeAll()`, `it()`, etc. Therefore, this must be run within a `describe()` block
+   */
+  function attemptC1WithNewState() {
+    // Each scenario uses a separate state and flowHelper because they fetch separate opportunities
+    const state = new RequestState(logger, uuid);
+    const flow = new FlowHelper(state);
+
+    beforeAll(async () => {
+      await state.fetchOpportunities(orderItemCriteria);
+      await chakram.wait();
+    });
+
+    (new GetMatch({
+      state, flow, logger, orderItemCriteria,
+    }))
+      .beforeSetup()
+      .successChecks()
+      .validationTests();
+    const c1 = (new C1({
+      state, flow, logger,
+    }))
+      .beforeSetup()
+      .successChecks()
+      .validationTests();
+
+    // Confirm that the booking system has the same opportunities as we gave
+    // it in our C1 request. This is especially important for the 2nd attempt,
+    // as this verifies that the order was successfully amended.
+    itFeedItemAndResponseItemShouldMatchIds(state, c1, () => state.c1Response);
+
+    return {
+      state, flow, c1,
+    };
+  }
+
   // N.B.: The following two tests must be performed sequentially - with
   // Second Attempt occurring after First Attempt.
   describe('First Attempt - C1', () => {
-    // Each of these scenarios uses a separate state and flowHelper because they fetch separate opportunities
-    const state = new RequestState(logger, uuid);
-    const flow = new FlowHelper(state);
-
-    beforeAll(async () => {
-      await state.fetchOpportunities(orderItemCriteria);
-      await chakram.wait();
-    });
-
-    (new GetMatch({
-      state, flow, logger, orderItemCriteria,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-    const c1 = (new C1({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-
-    itFeedItemAndResponseItemShouldMatchIds(state, c1, () => state.c1Response);
+    attemptC1WithNewState();
   });
   /** Fetch some new opportunities and amend the existing order at C1, and then complete it */
   describe('Second Attempt - C1 -> B', () => {
-    // Each of these scenarios uses a separate state and flowHelper because they fetch separate opportunities
-    const state = new RequestState(logger, uuid);
-    const flow = new FlowHelper(state);
+    const { state, flow } = attemptC1WithNewState();
 
-    beforeAll(async () => {
-      await state.fetchOpportunities(orderItemCriteria);
-      await chakram.wait();
-    });
-
-    (new GetMatch({
-      state, flow, logger, orderItemCriteria,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-    const c1 = (new C1({
+    (new C2({
       state, flow, logger,
     }))
       .beforeSetup()
       .successChecks()
       .validationTests();
-
-    // This should verify that the order has now been amended
-    itFeedItemAndResponseItemShouldMatchIds(state, c1, () => state.c1Response);
 
     const b = (new B({
       state, flow, logger,
@@ -101,6 +104,7 @@ FeatureHelper.describeFeature(module, {
       .validationTests();
 
     // Again, the completed order should be using our 2nd batch of order items
+    // i.e. it should have been successfully amended.
     itFeedItemAndResponseItemShouldMatchIds(state, b, () => state.bResponse);
   });
 });
