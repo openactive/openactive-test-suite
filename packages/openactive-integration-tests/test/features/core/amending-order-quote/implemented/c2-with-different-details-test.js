647edb087db4c21c93cf2fd55fa2c214e1bbb25b
Luke Winship <luke.winship@googlemail.com>
2020-08-20 10:12:33 +0100

    feat(amending-order-quote): Add test: amend-c1-and-c2 (#149)
    * docs(amending-order-quote): Improve test descriptions
    
    Co-authored-by: Nick Evans <2616208+nickevansuk@users.noreply.github.com>

--- packages/openactive-integration-tests/test/features/core/amending-order-quote/implemented/c2-with-different-details-test.js
+++ packages/openactive-integration-tests/test/features/core/amending-order-quote/implemented/c2-with-different-details-test.js
@@ -13,7 +13,7 @@ FeatureHelper.describeFeature(module, {
   testFeatureImplemented: true,
   testIdentifier: 'c2-with-different-details',
   testName: 'Run C2 with different details from C1',
-  testDescription: 'Run C1 with opportunity A, then - with the same Order UUID - C2 with opportunity B, then run B. Opportunity B should be booked',
+  testDescription: 'Run C1 with X opportunities, then - with the same Order UUID - run C2 with Y opportunities, then run B. The resulting Order should include confirmed bookings for only Y opportunities',
   // The primary opportunity criteria to use for the primary OrderItem under test
   testOpportunityCriteria: 'TestOpportunityBookable',
   // The secondary opportunity criteria to use for multiple OrderItem tests
