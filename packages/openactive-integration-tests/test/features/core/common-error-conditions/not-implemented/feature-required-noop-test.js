e9a970d092d8241bc164807ac5ffc33fe8ad0a50
Luke Winship <luke.winship@gmail.com>
2020-08-20 12:11:22 +0100

    feat(common-error-conditions): Create required not-implemented test

---
+++ packages/openactive-integration-tests/test/features/core/common-error-conditions/not-implemented/feature-required-noop-test.js
@@ -0,0 +1,9 @@
+const { FeatureHelper } = require('../../../../helpers/feature-helper');
+
+FeatureHelper.describeRequiredFeature(module, {
+  testCategory: 'core',
+  testFeature: 'common-error-conditions',
+  testFeatureImplemented: false,
+  testIdentifier: 'feature-required-noop',
+  testName: 'Feature required',
+});
