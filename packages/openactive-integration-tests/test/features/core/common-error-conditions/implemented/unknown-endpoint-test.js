69d542dc89c553ae2aae2c8d477b7828bf8fa1a7
Luke Winship <luke.winship@gmail.com>
2020-09-03 13:50:47 +0100

    feat(common-error-conditions): Create unknown-endpoint test

---
+++ packages/openactive-integration-tests/test/features/core/common-error-conditions/implemented/unknown-endpoint-test.js
@@ -0,0 +1,25 @@
+const { expect } = require('chai');
+const { FeatureHelper } = require('../../../../helpers/feature-helper');
+const RequestHelper = require('../../../../helpers/request-helper');
+
+const { BOOKING_API_BASE } = global;
+
+FeatureHelper.describeFeature(module, {
+  testCategory: 'core',
+  testFeature: 'common-error-conditions',
+  testFeatureImplemented: true,
+  testIdentifier: 'unknown-endpoint',
+  testName: 'Expect an UnknownOrIncorrectEndpointError for requests to unknown endpoints',
+  testDescription: 'Send a request to an endpoint that does not exist, and expect an UnknownOrIncorrectEndpointError to be returned',
+},
+(configuration, orderItemCriteria, featureIsImplemented, logger) => {
+  describe.only('Unknown Endpoint', () => {
+    it('should return an UnknownOrIncorrectEndpointError', async () => {
+      const requestHelper = new RequestHelper(logger);
+      const response = await requestHelper.put('UnknownEndpoint', `${BOOKING_API_BASE}ordeeeeers/abc`, {}, { timeout: 10000 });
+      expect(response.response).to.have.property('statusCode', 404);
+      expect(response.body).to.have.property('@type', 'UnknownOrIncorrectEndpointError');
+      expect(response.body).to.have.property('@context');
+    });
+  });
+});
