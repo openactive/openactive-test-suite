9cdd8f584c8ad63124b631a2b4f9c9fb77c665cf
Luke Winship <luke.winship@gmail.com>
2020-09-07 14:23:19 +0100

    chore(integration-tests): ESLint templates

--- packages/openactive-integration-tests/test/templates/c2-req.js
+++ packages/openactive-integration-tests/test/templates/c2-req.js
@@ -1,50 +1,46 @@
-module.exports = data => {
-  return {
-    "@context": "https://openactive.io/",
-    "@type": "OrderQuote",
-    brokerRole: "https://openactive.io/AgentBroker",
-    broker: {
-      "@type": "Organization",
-      name: "MyFitnessApp",
-      url: "https://myfitnessapp.example.com",
-      description: "A fitness app for all the community",
-      logo: {
-        "@type": "ImageObject",
-        url: "http://data.myfitnessapp.org.uk/images/logo.png"
-      },
-      address: {
-        "@type": "PostalAddress",
-        streetAddress: "Alan Peacock Way",
-        addressLocality: "Village East",
-        addressRegion: "Middlesbrough",
-        postalCode: "TS4 3AE",
-        addressCountry: "GB"
-      }
+module.exports = data => ({
+  '@context': 'https://openactive.io/',
+  '@type': 'OrderQuote',
+  brokerRole: 'https://openactive.io/AgentBroker',
+  broker: {
+    '@type': 'Organization',
+    name: 'MyFitnessApp',
+    url: 'https://myfitnessapp.example.com',
+    description: 'A fitness app for all the community',
+    logo: {
+      '@type': 'ImageObject',
+      url: 'http://data.myfitnessapp.org.uk/images/logo.png',
     },
-    seller: {
-      "@type": "Organization",
-      "@id": `${data.sellerId}`
+    address: {
+      '@type': 'PostalAddress',
+      streetAddress: 'Alan Peacock Way',
+      addressLocality: 'Village East',
+      addressRegion: 'Middlesbrough',
+      postalCode: 'TS4 3AE',
+      addressCountry: 'GB',
     },
-    customer: {
-      "@type": "Person",
-      email: "geoffcapes@example.com",
-      telephone: "020 811 8055",
-      givenName: "Geoff",
-      familyName: "Capes"
+  },
+  seller: {
+    '@type': 'Organization',
+    '@id': `${data.sellerId}`,
+  },
+  customer: {
+    '@type': 'Person',
+    email: 'geoffcapes@example.com',
+    telephone: '020 811 8055',
+    givenName: 'Geoff',
+    familyName: 'Capes',
+  },
+  orderedItem: data.orderItems.map(orderItem => ({
+    '@type': 'OrderItem',
+    position: orderItem.position,
+    acceptedOffer: {
+      '@type': 'Offer',
+      '@id': `${orderItem.acceptedOffer['@id']}`,
     },
-    orderedItem: data.orderItems.map((orderItem) => {
-      return {
-        "@type": "OrderItem",
-        position: orderItem.position,
-        acceptedOffer: {
-          "@type": "Offer",
-          "@id": `${orderItem.acceptedOffer['@id']}`
-        },
-        orderedItem: {
-          "@type": `${orderItem.orderedItem['@type']}`,
-          "@id": `${orderItem.orderedItem['@id']}`
-        }
-      };
-    })
-  };
-};
+    orderedItem: {
+      '@type': `${orderItem.orderedItem['@type']}`,
+      '@id': `${orderItem.orderedItem['@id']}`,
+    },
+  })),
+});
