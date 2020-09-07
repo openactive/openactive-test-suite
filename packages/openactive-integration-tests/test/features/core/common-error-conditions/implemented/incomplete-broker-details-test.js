205658469b5a5bd3bf758c2bcc959d970898ff39
Luke Winship <luke.winship@gmail.com>
2020-09-07 17:30:45 +0100

    feat(common-error-conditions): Add incomplete-broker-details test (just C1)

---
+++ packages/openactive-integration-tests/test/features/core/common-error-conditions/implemented/incomplete-broker-details-test.js
@@ -0,0 +1,76 @@
+const { expect } = require('chai');
+const { FeatureHelper } = require('../../../../helpers/feature-helper');
+const { FlowHelper } = require('../../../../helpers/flow-helper');
+const { RequestState } = require('../../../../helpers/request-state');
+const { GetMatch, C1, C2, B } = require('../../../../shared-behaviours');
+
+/**
+ * @typedef {import('chakram').ChakramResponse} ChakramResponse
+ */
+
+FeatureHelper.describeFeature(module, {
+  testCategory: 'core',
+  testFeature: 'common-error-conditions',
+  testFeatureImplemented: true,
+  testIdentifier: 'incomplete-broker-details',
+  testName: 'Expect an IncompleteBrokerDetailsError when broker details are missing name',
+  testDescription: 'Run each of C1, C2 and B for a valid opportunity, with broker details incomplete (missing name), expecting an IncompleteBrokerDetailsError to be returned',
+  // The primary opportunity criteria to use for the primary OrderItem under test
+  testOpportunityCriteria: 'TestOpportunityBookable',
+  // The secondary opportunity criteria to use for multiple OrderItem tests
+  controlOpportunityCriteria: 'TestOpportunityBookable',
+},
+(configuration, orderItemCriteria, featureIsImplemented, logger) => {
+  /**
+   * Fetch some opportunities and get matching ones
+   *
+   * Note: This generates jest blocks like `beforeAll()`, `it()`, etc. Therefore, this must be run within a `describe()` block
+   *
+   * @param {RequestState} state
+   * @param {FlowHelper} state
+   */
+  function doFetchOpportunitiesAndGetMatches(state, flow) {
+    beforeAll(async () => {
+      await state.fetchOpportunities(orderItemCriteria);
+    });
+
+    describe('Get Opportunity Feed Items', () => {
+      (new GetMatch({
+        state, flow, logger, orderItemCriteria,
+      }))
+        .beforeSetup()
+        .successChecks()
+        .validationTests();
+    });
+  }
+
+  /**
+   * @param {() => ChakramResponse} getChakramResponse This is wrapped in a
+   *   function because the actual response won't be available until the
+   *   asynchronous before() block has completed.
+   */
+  function itShouldReturnAnIncompleteBrokerDetailsError(getChakramResponse) {
+    it('should return an IncompleteBrokerDetailsError', () => {
+      const chakramResponse = getChakramResponse();
+      expect(chakramResponse.response).to.have.property('statusCode', 400);
+      expect(chakramResponse.body).to.have.property('@type', 'IncompleteBrokerDetailsError');
+      expect(chakramResponse.body).to.have.property('@context');
+    });
+  }
+
+  describe('Incomplete Broker Details at C1', () => {
+    const state = new RequestState(logger, { c1ReqTemplateRef: 'noBrokerName' });
+    const flow = new FlowHelper(state);
+
+    doFetchOpportunitiesAndGetMatches(state, flow);
+
+    describe('C1', () => {
+      (new C1({
+        state, flow, logger,
+      }))
+        .beforeSetup();
+
+      itShouldReturnAnIncompleteBrokerDetailsError(() => state.c1Response);
+    });
+  });
+});
--- packages/openactive-integration-tests/test/templates/c1-req.js
+++ packages/openactive-integration-tests/test/templates/c1-req.js
@@ -1,39 +1,85 @@
-module.exports = data => ({
-  '@context': 'https://openactive.io/',
-  '@type': 'OrderQuote',
-  brokerRole: 'https://openactive.io/AgentBroker',
-  broker: {
-    '@type': 'Organization',
-    name: 'MyFitnessApp',
-    url: 'https://myfitnessapp.example.com',
-    description: 'A fitness app for all the community',
-    logo: {
-      '@type': 'ImageObject',
-      url: 'http://data.myfitnessapp.org.uk/images/logo.png',
+const { dissocPath } = require('ramda');
+
+/**
+ * @typedef {{
+ *   sellerId: string,
+ *   orderItems: {
+ *     position: number,
+ *     acceptedOffer: {
+ *       '@id': string,
+ *     },
+ *     orderedItem: {
+ *       '@type': string,
+ *       '@id': string,
+ *     },
+ *   }[],
+ * }} C1ReqTemplateData
+ */
+
+/**
+ * @param {C1ReqTemplateData} data
+ */
+function createStandardC1Req(data) {
+  return {
+    '@context': 'https://openactive.io/',
+    '@type': 'OrderQuote',
+    brokerRole: 'https://openactive.io/AgentBroker',
+    broker: {
+      '@type': 'Organization',
+      name: 'MyFitnessApp',
+      url: 'https://myfitnessapp.example.com',
+      description: 'A fitness app for all the community',
+      logo: {
+        '@type': 'ImageObject',
+        url: 'http://data.myfitnessapp.org.uk/images/logo.png',
+      },
+      address: {
+        '@type': 'PostalAddress',
+        streetAddress: 'Alan Peacock Way',
+        addressLocality: 'Village East',
+        addressRegion: 'Middlesbrough',
+        postalCode: 'TS4 3AE',
+        addressCountry: 'GB',
+      },
     },
-    address: {
-      '@type': 'PostalAddress',
-      streetAddress: 'Alan Peacock Way',
-      addressLocality: 'Village East',
-      addressRegion: 'Middlesbrough',
-      postalCode: 'TS4 3AE',
-      addressCountry: 'GB',
+    seller: {
+      '@type': 'Organization',
+      '@id': data.sellerId,
     },
-  },
-  seller: {
-    '@type': 'Organization',
-    '@id': `${data.sellerId}`,
-  },
-  orderedItem: data.orderItems.map(orderItem => ({
-    '@type': 'OrderItem',
-    position: orderItem.position,
-    acceptedOffer: {
-      '@type': 'Offer',
-      '@id': `${orderItem.acceptedOffer['@id']}`,
-    },
-    orderedItem: {
-      '@type': `${orderItem.orderedItem['@type']}`,
-      '@id': `${orderItem.orderedItem['@id']}`,
-    },
-  })),
-});
+    orderedItem: data.orderItems.map(orderItem => ({
+      '@type': 'OrderItem',
+      position: orderItem.position,
+      acceptedOffer: {
+        '@type': 'Offer',
+        '@id': `${orderItem.acceptedOffer['@id']}`,
+      },
+      orderedItem: {
+        '@type': `${orderItem.orderedItem['@type']}`,
+        '@id': `${orderItem.orderedItem['@id']}`,
+      },
+    })),
+  };
+}
+
+/**
+ * C2 request with missing customer.email
+ *
+ * @param {C1ReqTemplateData} data
+ */
+function createNoBrokerNameC1Req(data) {
+  const req = createStandardC1Req(data);
+  return dissocPath(['broker', 'name'], req);
+}
+
+const c1ReqTemplates = {
+  standard: createStandardC1Req,
+  noBrokerName: createNoBrokerNameC1Req,
+};
+
+/**
+ * @typedef {keyof typeof c1ReqTemplates} C1ReqTemplateRef Reference to a particular C1 Request template
+ */
+
+module.exports = {
+  c1ReqTemplates,
+};
