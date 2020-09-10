76107d7eb85101d55bebd2b1c9d17cf4b7f200bd
Civ Sivakumaran <civ@imin.co>
2020-09-10 16:17:40 +0100

    add orderitems to cancel request to pass request validation

--- packages/openactive-integration-tests/test/features/cancellation/customer-requested-cancellation/implemented/unknown-order-test.js
+++ packages/openactive-integration-tests/test/features/cancellation/customer-requested-cancellation/implemented/unknown-order-test.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
+const { GetMatch } = require('../../../../shared-behaviours');
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'cancellation',
@@ -9,9 +10,22 @@ FeatureHelper.describeFeature(module, {
   testName: 'Expect a UnknownOrderError for an Order that does not exist',
   testDescription: 'Runs Order Cancellation for a non-existent Order (with a fictional UUID), expecting an UnknownOrderError error to be returned',
 },
-(configuration, orderItemCriteria, featureIsImplemented, logger, state) => {
+(configuration, orderItemCriteria, featureIsImplemented, logger, state, flow) => {
   describe('Cancel Order', () => {
     it('should return a UnknownOrderError', async () => {
+      // Get Opportunity Feed Items that match criteria specified in describeFeature()
+      beforeAll(async () => {
+        await state.fetchOpportunities(orderItemCriteria);
+      });
+      describe('Get Opportunity Feed Items', () => {
+        (new GetMatch({
+          state, flow, logger, orderItemCriteria,
+        }))
+          .beforeSetup()
+          .successChecks()
+          .validationTests();
+      });
+
       const statePostCancellation = await state.cancelOrder();
       const cancelOrderResponse = statePostCancellation.uResponse;
       expect(cancelOrderResponse.response).to.have.property('statusCode', 404);
