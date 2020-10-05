2d051e156536899c5c497d7ee23c0ea9bc746bf8
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2020-10-05 16:23:08 +0100

    Fix for supportsMinimalProposalFlow

--- packages/test-interface-criteria/src/criteria/sharedConstraints.js
+++ packages/test-interface-criteria/src/criteria/sharedConstraints.js
@@ -5,7 +5,8 @@
 /** @type {OfferConstraint} */
 function supportsMinimalProposalFlow(offer) {
   return Array.isArray(offer.openBookingFlowRequirement)
-    && offer.openBookingFlowRequirement.includes('https://openactive.io/OpenBookingApproval');
+    && offer.openBookingFlowRequirement.includes('https://openactive.io/OpenBookingApproval')
+    && offer.openBookingFlowRequirement.length === 1;
 }
 
 module.exports = {
