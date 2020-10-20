b6a9c5d8c806800db518b73c6698497523477707
Luke Winship <luke.winship@gmail.com>
2020-10-20 15:26:35 +0100

    better approach, just building up magic as needed rather than starting in magicland

--- packages/openactive-integration-tests/test/features/approval/minimal-proposal/implemented/accept-proposal-book-test.js
+++ packages/openactive-integration-tests/test/features/approval/minimal-proposal/implemented/accept-proposal-book-test.js
@@ -65,73 +65,283 @@ FeatureHelper.describeFeature(module, {
   // # Examples
   //
   // ## 1. Simple
+  // ## 1.1. With no magic at all
   {
-    // The SimpleFlowBuilder constructor creates default values for uuid & sellerId (although they can be overridden)
-    const { fetchOpportunities, c1, c2, p, simulateSellerApproval, orderFeedUpdate, b } = (new SimpleFlowBuilder({ requestHelper, logger }))
-      .addFetchOpportunities(orderItemCriteriaList)
-      // The above (^) is just a bit of sugar so that we don't have to write the below (v) for each flow
-      // .add('fetchOpportunities', FetchOpportunitiesFlowStage.create, () => ({
-      //   additionalParams: {
-      //     orderItemCriteriaList
-      //   },
-      // })
-      .add('c1', C1FlowStage.create) // SimpleFlowBuilder injects requestHelper, logger into the stage creation params
-      .add('c2', C2FlowStage.create) // and it also uses the previous stage's output state (each is merged) as the input state for the next
-      .add('p', PFlowStage.create)
-      .add('simulateSellerApproval', TestInterfaceActionFlowStage.create, ({ p }) => ({
-        additionalParams: { // additionalParams is used to add/overwrite params that are used to create the flow stage
-          testName: 'Simulate Seller Approval (Test Interface Action)',
-          createActionFn: () => ({
-            type: 'test:SellerAcceptOrderProposalSimulateAction',
-            objectType: 'OrderProposal',
-            objectId: p.getResponse().body['@id'],
-          }),
-        },
-      }))
-      // special build step which sets up listen and collect stages and links them with the previous stage
-      .addOrderFeedUpdate('orderFeedUpdate', {
+    const uuid = createUuid();
+    const sellerId = getSellerId();
+    const fetchOpportunities = FetchOpportunitiesFlowStage.create({
+      orderItemCriteriaList,
+      requestHelper,
+      logger,
+    });
+    const c1 = C1FlowStage.create({
+      getInputState: () => ({
+        orderItems: fetchOpportunities.getOutput().orderItems,
+      }),
+      prerequisite: fetchOpportunities,
+      uuid,
+      sellerId,
+      requestHelper,
+      logger,
+    });
+    const c2 = C2FlowStage.create({
+      getInputState: () => ({
+        orderItems: fetchOpportunities.getOutput().orderItems,
+      }),
+      prerequisite: c1,
+      uuid,
+      sellerId,
+      requestHelper,
+      logger,
+    });
+    const p = PFlowStage.create({
+      getInputState: () => ({
+        orderItems: fetchOpportunities.getOutput().orderItems,
+        totalPaymentDue: c2.getOutput().totalPaymentDue,
+      }),
+      prerequisite: c2,
+      uuid,
+      sellerId,
+      requestHelper,
+      logger,
+    });
+    const listenForOrderFeedUpdate = OrderFeedUpdateFlowStage.createListener({
+      prerequisite: p,
+      uuid,
+      requestHelper,
+    });
+    const simulateSellerApproval = TestInterfaceActionFlowStage.create({
+      testName: 'Simulate Seller Approval (Test Interface Action)',
+      prerequisite: listenForOrderFeedUpdate,
+      createActionFn: () => ({
+        type: 'test:SellerAcceptOrderProposalSimulateAction',
+        objectType: 'OrderProposal',
+        objectId: p.getOutput().orderId,
+      }),
+      requestHelper,
+    });
+    const collectOrderFeedUpdate = OrderFeedUpdateFlowStage.createCollector({
+      getInputState: () => ({
+        getOrderFromOrderFeedPromise: listenForOrderFeedUpdate.getOutput().getOrderFromOrderFeedPromise,
+      }),
+      testName: 'Order Feed Update (after Simulate Seller Approval)',
+      prerequisite: simulateSellerApproval,
+      logger,
+    });
+    const b = BFlowStage.create({
+      getInputState: () => ({
+        orderItems: fetchOpportunities.getOutput().orderItems,
+        orderProposalVersion: p.getOutput().orderProposalVersion, // (this could also use collectOrderFeedUpdate.getOutput())
+        totalPaymentDue: p.getOutput().totalPaymentDue,
+      }),
+      prerequisite: collectOrderFeedUpdate,
+      logger,
+      requestHelper,
+    });
+  }
+  // ## 1.2. With simple substitutions / helper functions
+  {
+    // Just puts uuid, sellerId, requestHelper, logger into an object, with default values for uuid & sellerId
+    const defaultFlowStageParams = FlowStageUtils.createDefaultFlowStageParams({ requestHelper, logger });
+    const fetchOpportunities = FetchOpportunitiesFlowStage.create({
+      ...defaultFlowStageParams,
+      orderItemCriteriaList,
+    });
+    const c1 = C1FlowStage.create({
+      ...defaultFlowStageParams,
+      // getMergedInputState gets the output from fetchOpportunities, merging with its prerequisites
+      getInputState: FlowStageUtils.getMergedInputState(fetchOpportunities),
+      prerequisite: fetchOpportunities,
+    });
+    const c2 = C2FlowStage.create({
+      ...defaultFlowStageParams,
+      // Just a bit of sugar that creates the { getInputState: FlowStageUtils.getMergedInputState(stage), prerequisite: stage }
+      ...FlowStageUtils.prerequisiteAndMergeInputs(c1),
+    });
+    const p = PFlowStage.create({
+      ...defaultFlowStageParams,
+      ...FlowStageUtils.prerequisiteAndMergeInputs(c2),
+    });
+    // if we wanted to inject custom state into p, we could juse ditch the magic function e.g.:
+    //
+    // ```
+    // const p = PFlowStage.create({
+    //   ...defaultFlowStageParams,
+    //   getInputState: () => ({
+    //     orderItems: [/** ... */],
+    //     totalPaymentDue: 3.1,
+    //   }),
+    //   prerequisite: c2,
+    // });
+    // ```
+    //
+    // If we wanted to use the prerequisite state, but just override one field, we can just add an arg to the getMergedInputState function:
+    //
+    // ```
+    // getInputState: FlowStageUtils.getMergedInputState(c2, () => ({ totalPaymentDue: -1.5 }))
+    // ```
+
+    // OrderFeedUpdateFlowStage.wrap simply reduces the order feed update listener/collector boilerplate
+    // (name improvements welcomed :P).
+    // The returned tuple is [wrappedStage, collectOrderFeedUpdateStage].
+    // Yes, the listenForOrderFeedUpdateStage isn't returned as it's not needed for writing tests.
+    const [simulateSellerApproval, orderFeedUpdate] = OrderFeedUpdateFlowStage.wrap({
+      // FlowStage that is getting wrapped
+      wrappedStage: prerequisite => TestInterfaceActionFlowStage.create({
+        testName: 'Simulate Seller Approval (Test Interface Action)',
+        prerequisite,
+        createActionFn: () => ({
+          type: 'test:SellerAcceptOrderProposalSimulateAction',
+          objectType: 'OrderProposal',
+          objectId: p.getOutput().orderId,
+        }),
+        requestHelper,
+      }),
+      // Params for the Order Feed Update stages
+      orderFeedUpdateParams: {
+        ...defaultFlowStageParams,
+        prerequisite: p,
         testName: 'Order Feed Update (after Simulate Seller Approval)',
-      })
-      .add('b', BFlowStage.create);
+      },
+    });
+    const b = BFlowStage.create({
+      ...defaultFlowStageParams,
+      prerequisite: orderFeedUpdate,
+      getInputState: FlowStageUtils.getMergedInputState(p),
+    });
   }
   // ## 2. Edge Case - Standalone / Override args
   {
-    const { p } = (new SimpleFlowBuilder({ requestHelper, logger, uuid: 'abc' })) // we'll still use SimpleFlowBuilder, as it initializes uuid & sellerId and threads them into each stage's input state
-      .add('p', PFlowStage, () => ({
-        // `getAdditionalInputState` is a function that adds to the input state that SimpleFlowBuilder extracts from prerequisite stages
-        getAdditionalInputState: () => ({
-          orderItems: [/* ... */],
-          totalPaymentDue: 3.4,
-        }),
-        additionalParams: {
-          templateRef: 'non-standard',
-        },
-      }));
+    const defaultFlowStageParams = FlowStageUtils.createDefaultFlowStageParams({ requestHelper, logger });
+    const p = PFlowStage.create({
+      ...defaultFlowStageParams,
+      getInputState: () => ({
+        orderItems: [/* ... */],
+        totalPaymentDue: 1.23,
+      }),
+    });
   }
   // ## 3. Edge Case - Repetitions / out of order input state
   {
-    const { fetchOpportunities, c1A, c1B, c2 } = (new SimpleFlowBuilder({ requestHelper, logger }))
-      .addFetchOpportunities(orderItemCriteriaList)
-      .add('c1A', C1FlowStage.create)
-      .add('c1B', C1FlowStage.create)
-      .add('c2', C2FlowStage.create)
-      .add('b', BFlowStage.create, ({ c1A }) => ({ // it uses the result from c1A (for some reason)
-        getAdditionalInputState: () => ({
-          totalPaymentDue: c1A.getOutput().totalPaymentDue,
-        }),
-      }));
+    const defaultFlowStageParams = FlowStageUtils.createDefaultFlowStageParams({ requestHelper, logger });
+    const fetchOpportunities = FetchOpportunitiesFlowStage.create({
+      ...defaultFlowStageParams,
+      orderItemCriteriaList,
+    });
+    const c1A = C1FlowStage.create({
+      ...defaultFlowStageParams,
+      ...FlowStageUtils.prerequisiteAndMergeInputs(fetchOpportunities),
+    });
+    const c1B = C1FlowStage.create({
+      ...defaultFlowStageParams,
+      ...FlowStageUtils.prerequisiteAndMergeInputs(c1A),
+    });
+    const c2 = C2FlowStage.create({
+      ...defaultFlowStageParams,
+      ...FlowStageUtils.prerequisiteAndMergeInputs(c1B),
+    });
+    const b = BFlowStage.create({
+      ...defaultFlowStageParams,
+      prerequisite: c2,
+      // uses the result from c1A (for some reason)
+      getInputState: FlowStageUtils.getMergedInputState(c1A),
+    });
   }
   // ## 4. Edge Case - Stages in the wrong order
   {
-    const { fetchOpportunities, c1A, c1B, c2 } = (new SimpleFlowBuilder({ requestHelper, logger }))
-      .addFetchOpportunities(orderItemCriteriaList)
-      .add('c1', C1FlowStage.create)
-      .add('p', PFlowStage.create) // this will use totalPaymentDue from c1
-      .add('c2', C2FlowStage.create)
-      .add('b', BFlowStage.create, () => ({
-        getInputStateFrom: 'c1', // will use the merged state from fetchOpportunities & c1 - and no further
-      }));
+    const defaultFlowStageParams = FlowStageUtils.createDefaultFlowStageParams({ requestHelper, logger });
+    const fetchOpportunities = FetchOpportunitiesFlowStage.create({
+      ...defaultFlowStageParams,
+      orderItemCriteriaList,
+    });
+    const c1 = C1FlowStage.create({
+      ...defaultFlowStageParams,
+      ...FlowStageUtils.prerequisiteAndMergeInputs(fetchOpportunities),
+    });
+    const p = PFlowStage.create({
+      ...defaultFlowStageParams,
+      ...FlowStageUtils.prerequisiteAndMergeInputs(c1), // will use totalPaymentDue from c1
+    });
+    const c2 = C2FlowStage.create({
+      ...defaultFlowStageParams,
+      prerequisite: p, // happens after p..
+      getInputState: FlowStageUtils.getMergedInputState(c1), // ..but uses state from c1
+    });
+    const b = C2FlowStage.create({
+      ...defaultFlowStageParams,
+      prerequisite: c2, // happens after c2..
+      getInputState: FlowStageUtils.getMergedInputState(c1), // ..but uses state from c1
+    });
   }
+  // //
+  // // ## 1. Simple
+  // {
+  //   // The SimpleFlowBuilder constructor creates default values for uuid & sellerId (although they can be overridden)
+  //   const { fetchOpportunities, c1, c2, p, simulateSellerApproval, orderFeedUpdate, b } = (new SimpleFlowBuilder({ requestHelper, logger }))
+  //     .addFetchOpportunities(orderItemCriteriaList)
+  //     // The above (^) is just a bit of sugar so that we don't have to write the below (v) for each flow
+  //     // .add('fetchOpportunities', FetchOpportunitiesFlowStage.create, () => ({
+  //     //   additionalParams: {
+  //     //     orderItemCriteriaList
+  //     //   },
+  //     // })
+  //     .add('c1', C1FlowStage.create) // SimpleFlowBuilder injects requestHelper, logger into the stage creation params
+  //     .add('c2', C2FlowStage.create) // and it also uses the previous stage's output state (each is merged) as the input state for the next
+  //     .add('p', PFlowStage.create)
+  //     .add('simulateSellerApproval', TestInterfaceActionFlowStage.create, ({ p }) => ({
+  //       additionalParams: { // additionalParams is used to add/overwrite params that are used to create the flow stage
+  //         testName: 'Simulate Seller Approval (Test Interface Action)',
+  //         createActionFn: () => ({
+  //           type: 'test:SellerAcceptOrderProposalSimulateAction',
+  //           objectType: 'OrderProposal',
+  //           objectId: p.getResponse().body['@id'],
+  //         }),
+  //       },
+  //     }))
+  //     // special build step which sets up listen and collect stages and links them with the previous stage
+  //     .addOrderFeedUpdate('orderFeedUpdate', {
+  //       testName: 'Order Feed Update (after Simulate Seller Approval)',
+  //     })
+  //     .add('b', BFlowStage.create);
+  // }
+  // // ## 2. Edge Case - Standalone / Override args
+  // {
+  //   const { p } = (new SimpleFlowBuilder({ requestHelper, logger, uuid: 'abc' })) // we'll still use SimpleFlowBuilder, as it initializes uuid & sellerId and threads them into each stage's input state
+  //     .add('p', PFlowStage, () => ({
+  //       // `getAdditionalInputState` is a function that adds to the input state that SimpleFlowBuilder extracts from prerequisite stages
+  //       getAdditionalInputState: () => ({
+  //         orderItems: [/* ... */],
+  //         totalPaymentDue: 3.4,
+  //       }),
+  //       additionalParams: {
+  //         templateRef: 'non-standard',
+  //       },
+  //     }));
+  // }
+  // // ## 3. Edge Case - Repetitions / out of order input state
+  // {
+  //   const { fetchOpportunities, c1A, c1B, c2 } = (new SimpleFlowBuilder({ requestHelper, logger }))
+  //     .addFetchOpportunities(orderItemCriteriaList)
+  //     .add('c1A', C1FlowStage.create)
+  //     .add('c1B', C1FlowStage.create)
+  //     .add('c2', C2FlowStage.create)
+  //     .add('b', BFlowStage.create, ({ c1A }) => ({ // it uses the result from c1A (for some reason)
+  //       getAdditionalInputState: () => ({
+  //         totalPaymentDue: c1A.getOutput().totalPaymentDue,
+  //       }),
+  //     }));
+  // }
+  // // ## 4. Edge Case - Stages in the wrong order
+  // {
+  //   const { fetchOpportunities, c1, p, c2, b } = (new SimpleFlowBuilder({ requestHelper, logger }))
+  //     .addFetchOpportunities(orderItemCriteriaList)
+  //     .add('c1', C1FlowStage.create)
+  //     .add('p', PFlowStage.create) // this will use totalPaymentDue from c1
+  //     .add('c2', C2FlowStage.create)
+  //     .add('b', BFlowStage.create, () => ({
+  //       getInputStateFrom: 'c1', // will use the merged state from fetchOpportunities & c1 - and no further
+  //     }));
+  // }
   // const fetchOpportunities = FetchOpportunitiesFlowStage.create({
   //   // orderItemCriteriaList,
   //   logger,
