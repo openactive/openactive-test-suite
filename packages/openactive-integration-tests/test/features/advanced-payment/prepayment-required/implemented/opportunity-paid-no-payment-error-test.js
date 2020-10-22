2c40cd2135fba1f40bd2811fe1d5aadd1d8c226d
Luke Wilson <luke.wilson@toptal.com>
2020-10-22 15:55:03 +0100

    Add another test

--- packages/openactive-integration-tests/test/features/advanced-payment/prepayment-required/implemented/opportunity-paid-no-payment-error-test.js
+++ packages/openactive-integration-tests/test/features/advanced-payment/prepayment-required/implemented/opportunity-paid-no-payment-error-test.js
@@ -1,18 +1,18 @@
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
 const { multipleOpportunityCriteriaTemplate, errorTests } = require('../../prepayment-common');
 
-const testOpportunityCriteria = 'TestOpportunityBookablePaidPrepaymentOptional';
-const expectedPrepayment = 'https://openactive.io/Optional';
+const testOpportunityCriteria = 'TestOpportunityBookablePaidPrepaymentRequired';
+const expectedPrepayment = 'https://openactive.io/Required';
 const expectedError = 'MissingPaymentDetailsError';
 const bReqTemplateRef = 'incorrectOrderDueToMissingPaymentProperty';
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'advanced-payment',
-  testFeature: 'prepayment-optional',
+  testFeature: 'prepayment-required',
   testFeatureImplemented: true,
   testIdentifier: 'opportunity-paid-no-payment-error',
   testName: 'Opportunity paid (no payment error)',
-  testDescription: 'Opportunity paid, prepayment optional, no payment (error)',
+  testDescription: 'Opportunity paid, prepayment required, no payment (error)',
   testOpportunityCriteria,
   controlOpportunityCriteria: 'TestOpportunityBookable',
   // temporarily disable control in multiple mode until refactoring complete
