202c5fdf61c58fda9ffecd2e42c67414319d680b
Luke Wilson <luke.wilson@toptal.com>
2020-10-23 17:17:51 +0100

    Lint fixes

--- packages/openactive-integration-tests/test/templates/b-req.js
+++ packages/openactive-integration-tests/test/templates/b-req.js
@@ -319,23 +319,23 @@ function createBReqWithBusinessCustomer(data) {
 }
 
 function createNoAccountId(data) {
-  let req = createStandardPaidBReq(data);
+  const req = createStandardPaidBReq(data);
   return dissocPath(['payment', 'accountId'], req);
 }
 
 function createNoPaymentProviderId(data) {
-  let req = createStandardPaidBReq(data);
+  const req = createStandardPaidBReq(data);
   return dissocPath(['payment', 'paymentProviderId'], req);
 }
 
 function createInvalidAccountId(data) {
-  let req = createStandardPaidBReq(data);
+  const req = createStandardPaidBReq(data);
   req.payment.accountId = 'some rubbish';
   return req;
 }
 
 function createInvalidPaymentProviderId(data) {
-  let req = createStandardPaidBReq(data);
+  const req = createStandardPaidBReq(data);
   req.payment.paymentProviderId = 'some rubbish';
   return req;
 }
