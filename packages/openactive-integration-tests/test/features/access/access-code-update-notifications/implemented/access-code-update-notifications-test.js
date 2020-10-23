a910dda1dede26c9a744f28536e341e9a8a7c832
Luke Winship <luke.winship@gmail.com>
2020-10-23 11:17:52 +0100

    "access codes should have altered values" assertions

--- packages/openactive-integration-tests/test/features/access/access-code-update-notifications/implemented/access-code-update-notifications-test.js
+++ packages/openactive-integration-tests/test/features/access/access-code-update-notifications/implemented/access-code-update-notifications-test.js
@@ -1,3 +1,5 @@
+const { expect } = require('chai');
+const { zip } = require('lodash');
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
 const RequestHelper = require('../../../../helpers/request-helper');
 const {
@@ -22,7 +24,7 @@ FeatureHelper.describeFeature(module, {
   // even if some OrderItems don't require approval, the whole Order should
   controlOpportunityCriteria: 'TestOpportunityBookable',
 },
-(configuration, orderItemCriteriaList, featureIsImplemented, logger, state, flow) => {
+(configuration, orderItemCriteriaList, featureIsImplemented, logger) => {
   const requestHelper = new RequestHelper(logger);
 
   // ## Initiate Flow Stages
@@ -53,7 +55,7 @@ FeatureHelper.describeFeature(module, {
       totalPaymentDue: c2.getOutput().totalPaymentDue,
     }),
   });
-  const [simulateSellerCancellation, orderFeedUpdate] = OrderFeedUpdateFlowStageUtils.wrap({
+  const [simulateAccessCodeUpdate, orderFeedUpdate] = OrderFeedUpdateFlowStageUtils.wrap({
     wrappedStageFn: prerequisite => (new TestInterfaceActionFlowStage({
       ...defaultFlowStageParams,
       testName: 'Simulate Access Code Update (Test Interface Action)',
@@ -70,106 +72,37 @@ FeatureHelper.describeFeature(module, {
       testName: 'Orders Feed (after Simulate Access Code Update)',
     },
   });
-  // TODO TODO TODO finish
 
   // ## Set up tests
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(fetchOpportunities);
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(c1);
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(c2);
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(b);
-  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(simulateSellerCancellation);
+  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(simulateAccessCodeUpdate);
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(orderFeedUpdate, () => {
-    it('should have orderItemStatus: SellerCancelled', () => {
-      const orderItems = orderFeedUpdate.getOutput().httpResponse.body.data.orderedItem;
+    it('should have access codes with altered values', () => {
+      // original = before the AccessCodeUpdateSimulationAction was invoked
+      const originalOrderItems = b.getOutput().httpResponse.body.orderedItem;
+      // new = after the AccessCodeUpdateSimulationAction was invoked
+      const newOrderItems = orderFeedUpdate.getOutput().httpResponse.body.data.orderedItem;
       // As we'll be setting out expectations in an iteration, this test would
       // give a false positive if there were no items in `orderedItem`, so we
       // explicitly test that the OrderItems are present.
-      expect(orderItems).to.be.an('array').with.lengthOf(fetchOpportunities.getOutput().orderItems.length);
-      for (const orderItem of orderItems) {
-        expect(orderItem).to.have.property('orderItemStatus', 'https://openactive.io/SellerCancelled');
-        expect(orderItem).to.have.property('cancellationMessage').which.is.a('string');
+      expect(newOrderItems).to.be.an('array');
+      expect(newOrderItems).to.have.lengthOf.above(0);
+      expect(newOrderItems).to.have.lengthOf(orderItemCriteriaList.length);
+
+      for (const [originalOrderItem, newOrderItem] of zip(originalOrderItems, newOrderItems)) {
+        const originalAccessCodes = originalOrderItem.accessCode;
+        const newAccessCodes = newOrderItem.accessCode;
+
+        // we don't check that newAccessCodes has the same length as originalAccessCodes
+        // as the length could feasibly have changed from the AccessCodeUpdateSimulateAction
+        // All we need to do, therefore, is check that the codes, as a whole, are different.
+        const sortedOriginalAccessCodes = [...originalAccessCodes].sort();
+        const sortedNewAccessCodes = [...newAccessCodes].sort();
+        expect(sortedNewAccessCodes).to.not.deep.equal(sortedOriginalAccessCodes);
       }
     });
   });
-
-  beforeAll(async () => {
-    await state.fetchOpportunities(orderItemCriteria);
-  });
-
-  describe('Get Opportunity Feed Items', () => {
-    (new GetMatch({
-      state, flow, logger, orderItemCriteria,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('C1', () => {
-    (new C1({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('C2', () => {
-    (new C2({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('B', () => {
-    const resp = (new B({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('Simulate Access Code Update (Test Interface Action)', () => {
-    const response = (new TestInterfaceAction({
-      flow,
-      logger,
-      createActionFn: () => ({
-        type: 'test:AccessCodeUpdateSimulateAction',
-        objectType: 'Order',
-        objectId: state.bResponse.body['@id'],
-      }),
-      completedFlowStage: 'B',
-    }))
-      .beforeSetup()
-      .successChecks();
-  });
-
-  // TODO: Orders feed check
-  // describe('Orders Feed (after U)', () => {
-  //     const orderFeedUpdate = (new OrderFeedUpdate({
-  //       state,
-  //       flow,
-  //       logger,
-  //       ordersFeedMode: 'orders-feed-after-u',
-  //     }))
-  //       .beforeSetup()
-  //       .successChecks()
-  //       .validationTests();
-  //Test that accesscode is updated!
-  // it('Response should include accessCode array with appropriate fields (name and description) for each OrderItem', () => {
-  //   chai.expect(state.bResponse.body.orderedItem).to.be.an('array');
-
-  //   state.bResponse.body.orderedItem.forEach((orderItem, orderItemIndex) => {
-  //     chai.expect(orderItem.accessCode).to.be.an('array');
-
-  //     orderItem.accessCode.forEach((accessCode, accessCodeIndex) => {
-  //       chai.expect(state.bResponse.body).to.have.nested.property(`orderedItem[${orderItemIndex}].accessCode[${accessCodeIndex}].name`).that.is.a('string');
-  //       chai.expect(state.bResponse.body).to.have.nested.property(`orderedItem[${orderItemIndex}].accessCode[${accessCodeIndex}].description`).to.equal('updatedPinCode');
-  //     });
-  //   });
-  // });
-   // });
 });
