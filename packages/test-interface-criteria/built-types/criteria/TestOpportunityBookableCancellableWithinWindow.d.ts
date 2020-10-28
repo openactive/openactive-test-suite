840f2aeab63d397bfacff1aadcb399fdbcdce8a6
Luke Winship <luke.winship@gmail.com>
2020-10-28 16:03:18 +0000

    add edge case doc to TestOpportunityBookableCancellableWithinWindow

--- packages/test-interface-criteria/built-types/criteria/TestOpportunityBookableCancellableWithinWindow.d.ts
+++ packages/test-interface-criteria/built-types/criteria/TestOpportunityBookableCancellableWithinWindow.d.ts
@@ -1,4 +1,8 @@
 /**
+ * Note that this criteria will ALWAYS reject any event whose latestCancellationBeforeStartDate
+ * duration is less than 2 hours. This is because this will conflict with the
+ * `startDateMustBe2HrsInAdvance` constraint (from `InternalCriteriaFutureScheduledOpportunity`).
+ *
  * Implements https://openactive.io/test-interface#TestOpportunityBookableCancellableWithinWindow
  */
 export const TestOpportunityBookableCancellableWithinWindow: import("../types/Criteria").Criteria;
--- packages/test-interface-criteria/src/criteria/TestOpportunityBookableCancellableWithinWindow.js
+++ packages/test-interface-criteria/src/criteria/TestOpportunityBookableCancellableWithinWindow.js
@@ -2,6 +2,10 @@ const { TestOpportunityBookable } = require('./TestOpportunityBookable');
 const { createCriteria, mustBeWithinCancellationWindow } = require('./criteriaUtils');
 
 /**
+ * Note that this criteria will ALWAYS reject any event whose latestCancellationBeforeStartDate
+ * duration is less than 2 hours. This is because this will conflict with the
+ * `startDateMustBe2HrsInAdvance` constraint (from `InternalCriteriaFutureScheduledOpportunity`).
+ *
  * Implements https://openactive.io/test-interface#TestOpportunityBookableCancellableWithinWindow
  */
 const TestOpportunityBookableCancellableWithinWindow = createCriteria({
