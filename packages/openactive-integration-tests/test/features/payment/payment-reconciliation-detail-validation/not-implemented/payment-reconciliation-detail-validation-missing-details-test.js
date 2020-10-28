2c00c63e711717a12b517a37fed710ce7ed63862
Luke Winship <luke.winship@gmail.com>
2020-10-28 11:06:20 +0000

    rename "missing-accountid" not-implemented test

--- packages/openactive-integration-tests/test/features/payment/payment-reconciliation-detail-validation/not-implemented/payment-reconciliation-detail-validation-missing-details-test.js
+++ packages/openactive-integration-tests/test/features/payment/payment-reconciliation-detail-validation/not-implemented/payment-reconciliation-detail-validation-missing-details-test.js
@@ -5,7 +5,7 @@ FeatureHelper.describeFeature(module, {
   testCategory: 'payment',
   testFeature: 'payment-reconciliation-detail-validation',
   testFeatureImplemented: false,
-  testIdentifier: 'payment-reconciliation-detail-validation-missing-accountId',
+  testIdentifier: 'payment-reconciliation-detail-validation-missing-reconciliation-details',
   testName: 'Payment reconciliation detail validation - missing reconciliation details',
   testDescription: 'C1, C2 and B - with missing reconciliation details - should succeed, ignoring these values',
   testOpportunityCriteria: 'TestOpportunityBookablePaid',
