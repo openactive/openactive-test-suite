4c539a6a8c3bc6e2ae5d1b2d364ada812ba471bc
Luke Winship <luke.winship@gmail.com>
2020-11-03 18:38:45 +0000

    Update book-and-seller-replace-all-items to check order feed and ensure that OrderItem IDs have changed

--- packages/openactive-integration-tests/test/features/cancellation/seller-requested-replacement/implemented/book-and-seller-replace-all-items-test.js
+++ packages/openactive-integration-tests/test/features/cancellation/seller-requested-replacement/implemented/book-and-seller-replace-all-items-test.js
@@ -1,6 +1,7 @@
+const { expect } = require('chai');
+const { prop } = require('ramda');
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
-const RequestHelper = require('../../../../helpers/request-helper');
-const { GetMatch, C1, C2, TestInterfaceAction, B } = require('../../../../shared-behaviours');
+const { TestRecipes } = require('../../../../shared-behaviours/test-recipes');
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'cancellation',
@@ -12,61 +13,24 @@ FeatureHelper.describeFeature(module, {
   testOpportunityCriteria: 'TestOpportunityBookable',
   controlOpportunityCriteria: 'TestOpportunityBookable',
 },
-(configuration, orderItemCriteria, featureIsImplemented, logger, state, flow) => {
-  this.requestHelper = new RequestHelper(logger);
+TestRecipes.simulateActionAndExpectOrderFeedUpdateAfterSimpleC1C2B({ actionType: 'test:ReplacementSimulateAction' },
+  ({ b, orderFeedUpdate, orderItemCriteriaList }) => {
+    it('should have replaced (at least one) OrderItems with a new one', () => {
+      // original = before the AccessPassUpdateSimulationAction was invoked
+      const originalOrderItems = b.getOutput().httpResponse.body.orderedItem;
+      // new = after the AccessPassUpdateSimulationAction was invoked
+      const newOrderItems = orderFeedUpdate.getOutput().httpResponse.body.data.orderedItem;
+      // As we'll be setting out expectations in an iteration, this test would
+      // give a false positive if there were no items in `orderedItem`, so we
+      // explicitly test that the OrderItems are present.
+      expect(newOrderItems).to.be.an('array')
+        .and.to.have.lengthOf.above(0)
+        .and.to.have.lengthOf(orderItemCriteriaList.length);
 
-  beforeAll(async () => {
-    await state.fetchOpportunities(orderItemCriteria);
-  });
-
-  describe('Get Opportunity Feed Items', () => {
-    (new GetMatch({
-      state, flow, logger, orderItemCriteria,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('C1', () => {
-    (new C1({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('C2', () => {
-    (new C2({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('B', () => {
-    (new B({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('Simulate Seller Replacement (Test Interface Action)', () => {
-    (new TestInterfaceAction({
-      flow,
-      logger,
-      createActionFn: () => ({
-        type: 'test:ReplacementSimulateAction',
-        objectType: 'Order',
-        objectId: state.bResponse.body['@id'],
-      }),
-      completedFlowStage: 'B',
-    }))
-      .beforeSetup()
-      .successChecks();
-  });
-});
+      // At least one OrderItem should be replaced with a new one.
+      // OrderItem IDs are sorted so that they can be directly compared with one another.
+      const originalOrderItemIdsSorted = originalOrderItems.map(prop('@id')).sort();
+      const newOrderItemIdsSorted = newOrderItems.map(prop('@id')).sort();
+      expect(originalOrderItemIdsSorted).to.not.deep.equal(newOrderItemIdsSorted);
+    });
+  }));
