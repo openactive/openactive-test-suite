4883d922babb37cadc0729507c5cad1cf7186eea
Luke Wilson <luke.wilson@toptal.com>
2020-11-06 16:19:13 +0000

    net tests

--- packages/openactive-integration-tests/test/features/tax/business-to-consumer-tax-calculation-net/implemented/business-to-consumer-tax-calculation-net-test.js
+++ packages/openactive-integration-tests/test/features/tax/business-to-consumer-tax-calculation-net/implemented/business-to-consumer-tax-calculation-net-test.js
@@ -3,10 +3,10 @@ const { netTest } = require('../../common');
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'tax',
-  testFeature: 'business-to-business-tax-calculation-net',
+  testFeature: 'business-to-consumer-tax-calculation-net',
   testFeatureImplemented: true,
-  testIdentifier: 'business-to-business-tax-calculation-net',
-  testName: 'Business-to-business tax calculation (net)',
+  testIdentifier: 'business-to-consumer-tax-calculation-net',
+  testName: 'Business-to-consumer tax calculation (net)',
   testDescription: 'The totals of totalPaymentTax should match the sum of the unitTaxSpecification, and the totalPaymentDue.price should equal the total of all acceptedOffer.price PLUS TotalPaymentTax.price.',
   singleOpportunityCriteriaTemplate: opportunityType => [
     {
@@ -26,4 +26,4 @@ FeatureHelper.describeFeature(module, {
   ],
   controlOpportunityCriteria: 'TestOpportunityBookable',
 },
-netTest({ c2ReqTemplateRef: 'businessCustomer', bReqTemplateRef: 'businessCustomer' }));
+netTest());
