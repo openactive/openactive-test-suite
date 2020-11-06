1a5372380dcc1afacc318909a695e6223cb7bd7d
Luke Wilson <luke.wilson@toptal.com>
2020-11-06 13:26:44 +0000

    codegen

---
+++ packages/test-interface-criteria/src/criteria/TestOpportunityBookableNonFreeTaxNet.js
@@ -0,0 +1,39 @@
+const { createCriteria } = require('./criteriaUtils');
+const { TestOpportunityBookableUsingPayment } = require('./TestOpportunityBookableUsingPayment');
+
+/**
+ * @typedef {import('../types/Criteria').OpportunityConstraint} OpportunityConstraint
+ */
+
+/**
+ * @type {OpportunityConstraint}
+ */
+function sellerTaxModeNet(opportunity) {
+  switch (opportunity['@type']) {
+    case 'ScheduledSession':
+      return opportunity.superEvent.organizer.taxMode === 'https://openactive.io/TaxNet';
+    case 'Slot':
+      return opportunity.facilityUse.provider.taxMode === 'https://openactive.io/TaxNet';
+    default:
+      throw new Error(`Type ${opportunity['@type']} not supported`);
+  }
+}
+
+/**
+ * Implements https://openactive.io/test-interface#TestOpportunityBookableNonFreeTaxNet.
+ */
+const TestOpportunityBookableNonFreeTaxNet = createCriteria({
+  name: 'TestOpportunityBookableNonFreeTaxNet',
+  opportunityConstraints: [
+    [
+      'Seller Tax Mode Net',
+      sellerTaxModeNet,
+    ],
+  ],
+  offerConstraints: [],
+  includeConstraintsFromCriteria: TestOpportunityBookableUsingPayment,
+});
+
+module.exports = {
+  TestOpportunityBookableNonFreeTaxNet,
+};
