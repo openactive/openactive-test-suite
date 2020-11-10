6bec4411cecbc9ecf9387f045ceab0776796b194
Luke Winship <luke.winship@gmail.com>
2020-11-10 15:27:45 +0000

    business-to-consumer gross

--- packages/openactive-integration-tests/test/features/tax/business-to-consumer-tax-calculation-gross/implemented/business-to-consumer-tax-calculation-gross-test.js
+++ packages/openactive-integration-tests/test/features/tax/business-to-consumer-tax-calculation-gross/implemented/business-to-consumer-tax-calculation-gross-test.js
@@ -9,6 +9,10 @@ FeatureHelper.describeFeature(module, {
   testName: 'Tax calculations',
   testDescription: 'The totals of totalPaymentTax should match the sum of the unitTaxSpecification, and the totalPaymentDue.price should equal the total of all acceptedOffer.price PLUS TotalPaymentTax.price.',
   testOpportunityCriteria: 'TestOpportunityBookableNonFreeTaxGross',
-  controlOpportunityCriteria: 'TestOpportunityBookable',
+  // the simple tests can only work if all OrderItems have the same tax mode
+  controlOpportunityCriteria: 'TestOpportunityBookableNonFreeTaxGross',
 },
+// The default C2/B request templates use a Person as customer, meaning that we
+// don't have to specify template overrides in order to get business-to-consumer
+// behaviour.
 grossTest());
