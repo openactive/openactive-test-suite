1516fd9b0f9220f042a877ae9deaac6d421e562e
Luke Winship <luke.winship@gmail.com>
2020-11-11 14:48:22 +0000

    bs terms of service

--- packages/openactive-integration-tests/test/features/terms/terms-of-service/implemented/booking-system-terms-of-service-test.js
+++ packages/openactive-integration-tests/test/features/terms/terms-of-service/implemented/booking-system-terms-of-service-test.js
@@ -1,9 +1,27 @@
-/* eslint-disable no-unused-vars */
 const chai = require('chai');
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
 const { FlowStageRecipes, FlowStageUtils } = require('../../../../helpers/flow-stages');
-chai.use(require('chai-arrays'));
 chai.use(require('chai-url'));
+
+/**
+ * @typedef {import('chakram').ChakramResponse} ChakramResponse
+ */
+
+/**
+ * @param {() => ChakramResponse} getHttpResponse
+ */
+function itShouldValidContainTermsOfService(getHttpResponse) {
+  it('Should contain terms of service array in bookingService in response', () => {
+    const { termsOfService } = getHttpResponse().body.bookingService;
+    chai.expect(termsOfService).to.be.an('array')
+      .that.has.lengthOf.at.least(1);
+    for (const termsOfServiceItem of termsOfService) {
+      // @ts-expect-error chai-url doesn't have a types package
+      chai.expect(termsOfServiceItem.url).that.has.protocol('https');
+      chai.expect(termsOfServiceItem['@type'] === 'PrivacyPolicy');
+    }
+  });
+}
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'terms',
@@ -23,31 +41,13 @@ function (configuration, orderItemCriteriaList, featureIsImplemented, logger) {
   describe('Terms of service should be part of bookingService in all stages', () => {
     FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(fetchOpportunities);
     FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(c1, () => {
-      it('Should contain terms of service array in bookingService in C1 response', () => {
-        c1.getOutput().httpResponse.body.bookingService.termsOfService.forEach((termOfService) => {
-          // @ts-expect-error chai-arrays doesn't have a types package
-          chai.expect(termOfService.url).that.has.protocol('https');
-          chai.expect(termOfService['@type'] === 'PrivacyPolicy');
-        });
-      });
+      itShouldValidContainTermsOfService(() => c1.getOutput().httpResponse);
     });
     FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(c2, () => {
-      it('Should contain terms of service array in bookingService in C2 response', () => {
-        c2.getOutput().httpResponse.body.bookingService.termsOfService.forEach((termOfService) => {
-          // @ts-expect-error chai-arrays doesn't have a types package
-          chai.expect(termOfService.url).that.has.protocol('https');
-          chai.expect(termOfService['@type'] === 'PrivacyPolicy');
-        });
-      });
+      itShouldValidContainTermsOfService(() => c2.getOutput().httpResponse);
     });
-    FlowStageUtils.describeRunAndCheckIsValid(b, () => {
-      it('Should contain terms of service array in bookingService in B response', () => {
-        b.getOutput().httpResponse.body.bookingService.termsOfService.forEach((termOfService) => {
-          // @ts-expect-error chai-arrays doesn't have a types package
-          chai.expect(termOfService.url).that.has.protocol('https');
-          chai.expect(termOfService['@type'] === 'PrivacyPolicy');
-        });
-      });
+    FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(b, () => {
+      itShouldValidContainTermsOfService(() => b.getOutput().httpResponse);
     });
   });
 });
