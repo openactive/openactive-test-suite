b755a1dc8798e6829e02614095c94eba984c7a0a
Luke Winship <luke.winship@gmail.com>
2020-11-12 10:54:21 +0000

    orders-updated-then-deleted & broker /get-order/ now works for deletes. But, the delete phase is skipped for now as https://github.com/openactive/OpenActive.Server.NET/issues/87 is required

--- packages/openactive-integration-tests/test/features/core/order-deletion/implemented/orders-updated-then-deleted-test.js
+++ packages/openactive-integration-tests/test/features/core/order-deletion/implemented/orders-updated-then-deleted-test.js
@@ -1,19 +1,12 @@
-/* eslint-disable no-unused-vars */
 const { expect } = require('chai');
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
 const {
-  FetchOpportunitiesFlowStage,
-  C1FlowStage,
-  C2FlowStage,
   FlowStageUtils,
   TestInterfaceActionFlowStage,
   OrderFeedUpdateFlowStageUtils,
-  BFlowStage,
   FlowStageRecipes,
 } = require('../../../../helpers/flow-stages');
 const { OrderDeletionFlowStage } = require('../../../../helpers/flow-stages/order-deletion');
-const RequestHelper = require('../../../../helpers/request-helper');
-const { itShouldReturnAnOpenBookingError } = require('../../../../shared-behaviours/errors');
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'core',
@@ -21,16 +14,16 @@ FeatureHelper.describeFeature(module, {
   testFeatureImplemented: true,
   testIdentifier: 'orders-updated-then-deleted',
   testName: 'Order successfully deleted',
-  testDescription: 'Run C1, C2 and B, and then check Orders feed for Order, then run U, then run Order Deletion, and check Orders feed that Order has been deleted',
+  testDescription: 'Run C1, C2 and B, and then check Orders feed for Order, then cancel it, then run Order Deletion, and check Orders feed that Order has been deleted',
   // The primary opportunity criteria to use for the primary OrderItem under test
   testOpportunityCriteria: 'TestOpportunityBookable',
   controlOpportunityCriteria: 'TestOpportunityBookable',
 },
-(configuration, orderItemCriteriaList, featureIsImplemented, logger, state) => {
+(configuration, orderItemCriteriaList, featureIsImplemented, logger) => {
   // ## Initiate Flow Stages
   const { fetchOpportunities, c1, c2, b, defaultFlowStageParams } = FlowStageRecipes.initialiseSimpleC1C2BFlow(orderItemCriteriaList, logger);
 
-  const [simulateSellerCancellation, orderFeedUpdate] = OrderFeedUpdateFlowStageUtils.wrap({
+  const [simulateSellerCancellation, orderFeedUpdateAfterCancel] = OrderFeedUpdateFlowStageUtils.wrap({
     wrappedStageFn: prerequisite => (new TestInterfaceActionFlowStage({
       ...defaultFlowStageParams,
       testName: 'Simulate Seller Cancellation (Test Interface Action)',
@@ -48,15 +41,14 @@ FeatureHelper.describeFeature(module, {
     },
   });
 
-  // TODO: I tried capturing orderFeedUpdate again, but deleted orders won't show there anyway. So maybe OrderDeletionFlowStage is not needed it can be simplified with last commented check.
-  const [deleteStage, orderFeedUpdate2] = OrderFeedUpdateFlowStageUtils.wrap({
+  const [deleteOrder, orderFeedUpdateAfterDelete] = OrderFeedUpdateFlowStageUtils.wrap({
     wrappedStageFn: prerequisite => (new OrderDeletionFlowStage({
       ...defaultFlowStageParams,
-      prerequisite: orderFeedUpdate,
+      prerequisite,
     })),
     orderFeedUpdateParams: {
       ...defaultFlowStageParams,
-      prerequisite: b,
+      prerequisite: orderFeedUpdateAfterCancel,
       testName: 'Orders Feed (after OrderDeletion)',
     },
   });
@@ -67,9 +59,9 @@ FeatureHelper.describeFeature(module, {
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(c2);
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(b);
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(simulateSellerCancellation);
-  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(orderFeedUpdate, () => {
+  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(orderFeedUpdateAfterCancel, () => {
     it('should have orderItemStatus: SellerCancelled', () => {
-      const orderItems = orderFeedUpdate.getOutput().httpResponse.body.data.orderedItem;
+      const orderItems = orderFeedUpdateAfterCancel.getOutput().httpResponse.body.data.orderedItem;
       expect(orderItems).to.be.an('array').with.lengthOf(orderItemCriteriaList.length);
       for (const orderItem of orderItems) {
         expect(orderItem).to.have.property('orderItemStatus', 'https://openactive.io/SellerCancelled');
@@ -77,23 +69,12 @@ FeatureHelper.describeFeature(module, {
       }
     });
   });
-
-  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(deleteStage, () => {
-    it('should have deleted order and return 204 NoContent status code', async () => {
-      const apiResponse = deleteStage.getOutput().httpResponse;
-      expect(apiResponse.response.statusCode).to.equal(204);
+  describe.skip('un-skip this once https://github.com/openactive/OpenActive.Server.NET/issues/87 is completed', () => {
+    FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(deleteOrder);
+    FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(orderFeedUpdateAfterDelete, () => {
+      it('should have state: deleted', () => {
+        expect(orderFeedUpdateAfterDelete.getOutput().httpResponse.body.state).to.equal('deleted');
+      });
     });
   });
-
-  // TODO: COnfirm in Orders feed that it is deleted?
-
-  // describe('Delete Order ', async () => {
-  //   it('get order by uuid', async () => {
-  //     const apiResponse = await requestHelper.getOrderStatus(defaultFlowStageParams.uuid, { sellerId: defaultFlowStageParams.sellerId });
-  //     // give a false positive if there were no items in `orderedItem`, so we
-  //     // explicitly test that the OrderItems are present.
-  //     expect(apiResponse.response.statusCode).to.equal(204);
-  //   });
-  //   const apiResponse = await requestHelper.deleteOrderQuote(defaultFlowStageParams.uuid, { sellerId: defaultFlowStageParams.sellerId });
-  // });
 });
