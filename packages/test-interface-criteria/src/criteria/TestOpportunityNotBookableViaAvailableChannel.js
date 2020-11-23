05ad59890f9e521a383e8db9b224eb15647747d6
Luke Winship <luke.winship@gmail.com>
2020-11-23 12:23:02 +0000

    apply to all criteria

--- packages/test-interface-criteria/src/criteria/TestOpportunityNotBookableViaAvailableChannel.js
+++ packages/test-interface-criteria/src/criteria/TestOpportunityNotBookableViaAvailableChannel.js
@@ -1,5 +1,6 @@
 const { InternalCriteriaFutureScheduledOpportunity } = require('./internal/InternalCriteriaFutureScheduledOpportunity');
 const { createCriteria } = require('./criteriaUtils');
+const { testOfferDataRequirements, availableChannelArrayRequirements } = require('../testDataRequirements');
 
 /**
  * @typedef {import('../types/Criteria').OfferConstraint} OfferConstraint
@@ -25,7 +26,11 @@ const TestOpportunityNotBookableViaAvailableChannel = createCriteria({
     ],
   ],
   testDataRequirements: () => ({
-    availableChannelExcludes: 'https://openactive.io/OpenBookingPrepayment',
+    'test:testOfferDataRequirements': testOfferDataRequirements({
+      'test:availableChannel': availableChannelArrayRequirements({
+        excludesAll: ['https://openactive.io/OpenBookingPrepayment'],
+      }),
+    }),
   }),
   includeConstraintsFromCriteria: InternalCriteriaFutureScheduledOpportunity,
 });
