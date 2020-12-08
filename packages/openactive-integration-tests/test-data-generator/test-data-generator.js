73c27ee698e0be225e7b502462d715d503764a6b
Luke Winship <luke.winship@gmail.com>
2020-12-08 11:46:40 +0000

    test-data-generator uses category identifier

--- packages/openactive-integration-tests/test-data-generator/test-data-generator.js
+++ packages/openactive-integration-tests/test-data-generator/test-data-generator.js
@@ -12,6 +12,7 @@ const { getSellerConfigFromSellerCriteria } = require('../test/helpers/sellers')
 const { createTestInterfaceOpportunity } = require('../test/helpers/test-interface-opportunities');
 
 /**
+ * @typedef {import('../documentation/generator').CategoriesJson} CategoriesJson
  * @typedef {import('../documentation/generator').CriteriaRequirementsJson} CriteriaRequirementsJson
  * @typedef {import('../test/types/TestInterfaceOpportunity').TestInterfaceOpportunity} TestInterfaceOpportunity
  * @typedef {import('../test/types/OpportunityCriteria').SellerCriteria} SellerCriteria
@@ -37,6 +38,7 @@ const { createTestInterfaceOpportunity } = require('../test/helpers/test-interfa
 
 // # Constants - File Paths
 const CRITERIA_REQUIREMENTS_JSON_FILE_PATH = path.join(__dirname, '..', 'test', 'features', 'criteria-requirements.json');
+const CATEGORIES_JSON_FILE_PATH = path.join(__dirname, '..', 'test', 'features', 'categories.json');
 const DEFAULT_OUTPUT_FILE_PATH = path.join(__dirname, 'test-data', 'test-data.json');
 
 // # Constants - Config
@@ -49,10 +51,10 @@ const IMPLEMENTED_OPPORTUNITY_TYPES = Object.entries(BOOKABLE_OPPORTUNITY_TYPES_
 
 // # Process CLI Args
 const argv = yargs(process.argv.slice(2)) // eslint-disable-line prefer-destructuring
-  .command('$0 [feature]', 'OpenActive Test Data Generator', (yargsConfig) => {
-    yargsConfig.positional('feature', {
+  .command('$0 [category-or-feature]', 'OpenActive Test Data Generator', (yargsConfig) => {
+    yargsConfig.positional('category-or-feature', {
       type: 'string',
-      describe: 'If selected, only generate test data for this Feature (e.g. agent-broker)',
+      describe: 'If included, only generate test data for this Category (e.g. core) or Feature (e.g. agent-broker)',
       default: '*',
     });
   })
@@ -66,7 +68,8 @@ const argv = yargs(process.argv.slice(2)) // eslint-disable-line prefer-destruct
   })
   .argv;
 
-const { output: outputFilePath, feature } = argv;
+const { output: outputFilePath, 'category-or-feature': categoryOrFeatureUntyped } = argv;
+const categoryOrFeature = /** @type {string} */(categoryOrFeatureUntyped); // yargs can only properly type the option args - not the positional ones. Hence the TS coercion here
 
 // # Main
 
@@ -80,7 +83,7 @@ const { output: outputFilePath, feature } = argv;
   // ## Tally the requirements from each implemented feature
   const sellerRequirements = tallySellerCriteriaRequirements(
     criteriaRequirementsJson,
-    /** @type {string} */(feature), // yargs can only properly type the option args - not the positional ones. Hence the TS coercion here
+    (await getSelectedFeatureIdentifiers(categoryOrFeature)),
   );
 
   // ## Create Test Data
@@ -124,34 +127,129 @@ const { output: outputFilePath, feature } = argv;
 // # Utils
 
 /**
+ * Count opportunity requirements for each seller, given the features which are selected and implemented
+ *
  * @param {CriteriaRequirementsJson} criteriaRequirementsJson
- * @param {string} [onlyThisFeatureIdentifier]
- *   If included (and not '*'), only generates test data for this feature identifier
- *   If excluded, is equivalent to '*', in which case test data is generated for all features
+ * @param {string[]} featureIdentifiers Only generates test data for these feature identifiers.
  * @returns {SellerCriteriaRequirements}
  */
-function tallySellerCriteriaRequirements(criteriaRequirementsJson, onlyThisFeatureIdentifier = '*') {
-  if (onlyThisFeatureIdentifier && onlyThisFeatureIdentifier !== '*') {
-    const sellerCriteriaRequirementsObj = criteriaRequirementsJson.criteriaRequirements[onlyThisFeatureIdentifier];
-    if (!sellerCriteriaRequirementsObj) {
-      throw new Error(`Feature not found: "${onlyThisFeatureIdentifier}"`);
-    }
-    return getSellerCriteriaRequirementsFromJsonObj(sellerCriteriaRequirementsObj);
-  }
-  // All Features
+function tallySellerCriteriaRequirements(criteriaRequirementsJson, featureIdentifiers) {
   /**
    * The SellerCriteriaRequirements for every implemented features
    * @type {SellerCriteriaRequirements[]}
    */
   const sellerCriteriaRequirementMaps = [];
-  for (const [featureIdentifier, sellerCriteriaRequirementsObj] of Object.entries(criteriaRequirementsJson.criteriaRequirements)) {
-    // Feature isn't implemented, so we don't consider it
-    if (!IMPLEMENTED_FEATURES[featureIdentifier]) { continue; }
+  for (const featureIdentifier of featureIdentifiers) {
+    const sellerCriteriaRequirementsObj = criteriaRequirementsJson.criteriaRequirements[featureIdentifier];
     const sellerCriteriaRequirements = getSellerCriteriaRequirementsFromJsonObj(sellerCriteriaRequirementsObj);
     sellerCriteriaRequirementMaps.push(sellerCriteriaRequirements);
   }
   return SellerCriteriaRequirements.combine(sellerCriteriaRequirementMaps);
 }
+
+/**
+ * @param {string} [categoryOrFeatureIdentifier]
+ *   If included (and not '*'), only generates test data for this feature identifier
+ *   If excluded, is equivalent to '*', in which case test data is generated for all features
+ * @returns {Promise<string[]>}
+ */
+async function getSelectedFeatureIdentifiers(categoryOrFeatureIdentifier) {
+  if (categoryOrFeatureIdentifier && categoryOrFeatureIdentifier !== '*') {
+    // **First, assume it's a category identifier.**
+    //
+    // Best to go with this first as, if a category and feature have the same name, it would
+    // be more frustrating to not be able to select the category than it would be to not be
+    // able to select the feature.
+    // Because if you cannot select a single category, you would have to just get test data
+    // for everything, which would make data generation take much longer for each test.
+    const categoriesJsonRaw = await fs.readFile(CATEGORIES_JSON_FILE_PATH);
+    /** @type {CategoriesJson} */
+    const categoriesJson = JSON.parse(categoriesJsonRaw.toString());
+    if (categoryOrFeatureIdentifier in categoriesJson.categories) {
+      // Return features in that category
+      return onlyIncludeImplementedFeatures(Object.keys(categoriesJson.categories[categoryOrFeatureIdentifier]));
+    }
+    // **Otherwise, it must be a feature identifier.**
+    return onlyIncludeImplementedFeatures([categoryOrFeatureIdentifier]);
+  }
+  // If no features have been selected, just use all the implemented features
+  return Object.entries(IMPLEMENTED_FEATURES)
+    .filter(([_, isImplemented]) => isImplemented) // eslint-disable-line no-unused-vars
+    .map(([featureIdentifier, _]) => featureIdentifier); // eslint-disable-line no-unused-vars
+}
+
+/**
+ * Narrow array of feature identifiers down so as to only include this which are implemented
+ *
+ * Note: This raises an error if none of the identifiers make it through the filter.
+ *
+ * @param {string[]} featureIdentifiers
+ */
+function onlyIncludeImplementedFeatures(featureIdentifiers) {
+  const implementedFeatureIdentifiers = featureIdentifiers.filter(featureIdentifier => IMPLEMENTED_FEATURES[featureIdentifier]);
+  if (implementedFeatureIdentifiers.length === 0) {
+    throw new Error(`None of the selected feature identifiers (${featureIdentifiers.join(', ')}) are implemented. Therefore, no test data can be generated.`);
+  }
+  return implementedFeatureIdentifiers;
+}
+
+// /**
+//  * ## Tally Seller Criteria Requirements
+//  *
+//  * Count opportunity requirements for each seller, given the features which are selected and implemented
+//  *
+//  * @param {CriteriaRequirementsJson} criteriaRequirementsJson
+//  * @param {string} [categoryOrFeatureIdentifier]
+//  *   If included (and not '*'), only generates test data for this feature identifier
+//  *   If excluded, is equivalent to '*', in which case test data is generated for all features
+//  * @returns {SellerCriteriaRequirements}
+//  */
+// function tallySellerCriteriaRequirementsUh(criteriaRequirementsJson, categoryOrFeatureIdentifier = '*') {
+//   if (categoryOrFeatureIdentifier && categoryOrFeatureIdentifier !== '*') {
+//     // ### First, assume it's a category identifier.
+//     //
+//     // Best to go with this first as, if a category and feature have the same name, it would
+//     // be more frustrating to not be able to select the category than it would be to not be
+//     // able to select the feature.
+//     // Because if you cannot select a single category, you would have to just get test data
+//     // for everything, which would make data generation take much longer for each test.
+//     if (categoryOrFeatureIdentifier in criteriaRequirementsJson.criteriaRequirements) {
+//       /**
+//        * The SellerCriteriaRequirements for every implemented features
+//        * @type {SellerCriteriaRequirements[]}
+//        */
+//       const sellerCriteriaRequirementMaps = [];
+//       for (const [featureIdentifier, sellerCriteriaRequirementsObj] of Object.entries(criteriaRequirementsJson.criteriaRequirements[categoryOrFeatureIdentifier])) {
+//         // Feature isn't implemented, so we don't consider it
+//         if (!IMPLEMENTED_FEATURES[featureIdentifier]) { continue; }
+//         const sellerCriteriaRequirements = getSellerCriteriaRequirementsFromJsonObj(sellerCriteriaRequirementsObj);
+//         sellerCriteriaRequirementMaps.push(sellerCriteriaRequirements);
+//       }
+//       return SellerCriteriaRequirements.combine(sellerCriteriaRequirementMaps);
+//     }
+//     // ### Otherwise, it's a feature identifier
+//     for (const [categoryIdentifier, featureRequirements] of Object.entries(criteriaRequirementsJson.criteriaRequirements)) {
+//     }
+//     const sellerCriteriaRequirementsObj = criteriaRequirementsJson.criteriaRequirements[categoryOrFeatureIdentifier];
+//     if (!sellerCriteriaRequirementsObj) {
+//       throw new Error(`Feature not found: "${categoryOrFeatureIdentifier}"`);
+//     }
+//     return getSellerCriteriaRequirementsFromJsonObj(sellerCriteriaRequirementsObj);
+//   }
+//   // ### For All Features
+//   /**
+//    * The SellerCriteriaRequirements for every implemented features
+//    * @type {SellerCriteriaRequirements[]}
+//    */
+//   const sellerCriteriaRequirementMaps = [];
+//   for (const [featureIdentifier, sellerCriteriaRequirementsObj] of Object.entries(criteriaRequirementsJson.criteriaRequirements)) {
+//     // Feature isn't implemented, so we don't consider it
+//     if (!IMPLEMENTED_FEATURES[featureIdentifier]) { continue; }
+//     const sellerCriteriaRequirements = getSellerCriteriaRequirementsFromJsonObj(sellerCriteriaRequirementsObj);
+//     sellerCriteriaRequirementMaps.push(sellerCriteriaRequirements);
+//   }
+//   return SellerCriteriaRequirements.combine(sellerCriteriaRequirementMaps);
+// }
 
 /**
  * @param {CriteriaRequirementsJson['criteriaRequirements'][string]} sellerCriteriaRequirementsObj
