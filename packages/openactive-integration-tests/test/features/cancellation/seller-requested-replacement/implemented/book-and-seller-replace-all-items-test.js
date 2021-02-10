2ec965c5056a69c096683fb64be94f2d731807b1
Civ Sivakumaran <civ@imin.co>
2021-02-10 18:23:23 +0000

    fix test

--- packages/openactive-integration-tests/test/features/cancellation/seller-requested-replacement/implemented/book-and-seller-replace-all-items-test.js
+++ packages/openactive-integration-tests/test/features/cancellation/seller-requested-replacement/implemented/book-and-seller-replace-all-items-test.js
@@ -1,5 +1,4 @@
 const { expect } = require('chai');
-const { prop } = require('ramda');
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
 const { TestRecipes } = require('../../../../shared-behaviours/test-recipes');
 
@@ -8,18 +7,21 @@ FeatureHelper.describeFeature(module, {
   testFeature: 'seller-requested-replacement',
   testFeatureImplemented: true,
   testIdentifier: 'book-and-seller-replace-all-items',
-  testName: 'Bookm seller replaces order items.',
+  testName: 'Book and seller replaces order items.',
   testDescription: 'A successful replacement of order items by seller.',
   testOpportunityCriteria: 'TestOpportunityBookable',
   controlOpportunityCriteria: 'TestOpportunityBookable',
 },
-TestRecipes.simulateActionAndExpectOrderFeedUpdateAfterSimpleC1C2B({ actionType: 'test:ReplacementSimulateAction' },
+TestRecipes.simulateActionAndExpectOrderFeedUpdateAfterSimpleC1C2B(
+  { actionType: 'test:ReplacementSimulateAction' },
   ({ b, orderFeedUpdate, orderItemCriteriaList }) => {
     it('should have replaced (at least one) OrderItems with a new one', () => {
       // original = before the AccessPassUpdateSimulationAction was invoked
       const originalOrderItems = b.getOutput().httpResponse.body.orderedItem;
+
       // new = after the AccessPassUpdateSimulationAction was invoked
       const newOrderItems = orderFeedUpdate.getOutput().httpResponse.body.data.orderedItem;
+
       // As we'll be setting out expectations in an iteration, this test would
       // give a false positive if there were no items in `orderedItem`, so we
       // explicitly test that the OrderItems are present.
@@ -27,10 +29,12 @@ TestRecipes.simulateActionAndExpectOrderFeedUpdateAfterSimpleC1C2B({ actionType:
         .and.to.have.lengthOf.above(0)
         .and.to.have.lengthOf(orderItemCriteriaList.length);
 
-      // At least one OrderItem should be replaced with a new one.
-      // OrderItem IDs are sorted so that they can be directly compared with one another.
-      const originalOrderItemIdsSorted = originalOrderItems.map(prop('@id')).sort();
-      const newOrderItemIdsSorted = newOrderItems.map(prop('@id')).sort();
-      expect(originalOrderItemIdsSorted).to.not.deep.equal(newOrderItemIdsSorted);
+      // At least one orderedItem with the OrderItems should be replaced with a new one
+      // orderedItems within OrderItems are sorted so that they can be directly compared with one another.
+      const originalOpportunityIdsSorted = originalOrderItems.map(orderItem => orderItem.orderedItem['@id']).sort();
+      const newOpportunityIdsSorted = newOrderItems.map(orderItem => orderItem.orderedItem['@id']).sort();
+
+      expect(originalOpportunityIdsSorted).to.not.deep.equal(newOpportunityIdsSorted);
     });
-  }));
+  },
+));
