cf6c02b0c9c26f8fd0bdba8403cbd106be87fd70
Civ Sivakumaran <civ@imin.co>
2021-02-15 15:19:52 +0000

    fix test

--- packages/openactive-integration-tests/test/features/core/common-error-conditions/implemented/incomplete-order-item-test.js
+++ packages/openactive-integration-tests/test/features/core/common-error-conditions/implemented/incomplete-order-item-test.js
@@ -1,33 +1,9 @@
-const chai = require('chai');
-const chakram = require('chakram');
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
 const {
   FlowStageUtils,
   FlowStageRecipes,
 } = require('../../../../helpers/flow-stages');
 const { itShouldReturnAnOpenBookingError } = require('../../../../shared-behaviours/errors');
-/**
- * @typedef {import('chakram').ChakramResponse} ChakramResponse
- */
-
-/**
- * @param {() => ChakramResponse} responseAccessor This is wrapped in a
- *   function because the actual response won't be available until the
- *   asynchronous before() block has completed.
- */
-function itIncompleteOrderItemErrorInsideOrderItems(responseAccessor) {
-  it('should return 409', () => {
-    chakram.expect(responseAccessor()).to.have.status(409);
-  });
-
-  it('should contain IncompleteOrderItemError', () => {
-    const resp = responseAccessor().body;
-    resp.orderedItem.forEach((orderedItem) => {
-      const responseOrderItemErrorTypes = orderedItem.error.map(e => e['@type']);
-      chai.expect(responseOrderItemErrorTypes).to.include('IncompleteOrderItemError');
-    });
-  });
-}
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'core',
@@ -50,12 +26,15 @@ FeatureHelper.describeFeature(module, {
 
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(fetchOpportunities);
   FlowStageUtils.describeRunAndRunChecks({ doCheckIsValid: false, doCheckSuccess: false }, c1, () => {
-    itIncompleteOrderItemErrorInsideOrderItems(() => c1.getOutput().httpResponse);
+    // When CR3 is accepted and merged into the Booking Spec, this will be a 400 error, not a 409
+    itShouldReturnAnOpenBookingError('IncompleteOrderItemError', 409, () => c1.getOutput().httpResponse);
   });
   FlowStageUtils.describeRunAndRunChecks({ doCheckIsValid: false, doCheckSuccess: false }, c2, () => {
-    itIncompleteOrderItemErrorInsideOrderItems(() => c2.getOutput().httpResponse);
+    // When CR3 is accepted and merged into the Booking Spec, this will be a 400 error, not a 409
+    itShouldReturnAnOpenBookingError('IncompleteOrderItemError', 409, () => c2.getOutput().httpResponse);
   });
   FlowStageUtils.describeRunAndCheckIsValid(b, () => {
+    // When CR3 is accepted and merged into the Booking Spec, this will be a 400 error, not a 409
     itShouldReturnAnOpenBookingError('IncompleteOrderItemError', 409, () => b.getOutput().httpResponse);
   });
 
@@ -67,12 +46,15 @@ FeatureHelper.describeFeature(module, {
 
   FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(second.fetchOpportunities);
   FlowStageUtils.describeRunAndRunChecks({ doCheckIsValid: false, doCheckSuccess: false }, second.c1, () => {
-    itIncompleteOrderItemErrorInsideOrderItems(() => second.c1.getOutput().httpResponse);
+    // When CR3 is accepted and merged into the Booking Spec, this will be a 400 error, not a 409
+    itShouldReturnAnOpenBookingError('IncompleteOrderItemError', 409, () => second.c1.getOutput().httpResponse);
   });
   FlowStageUtils.describeRunAndRunChecks({ doCheckIsValid: false, doCheckSuccess: false }, second.c2, () => {
-    itIncompleteOrderItemErrorInsideOrderItems(() => second.c2.getOutput().httpResponse);
+    // When CR3 is accepted and merged into the Booking Spec, this will be a 400 error, not a 409
+    itShouldReturnAnOpenBookingError('IncompleteOrderItemError', 409, () => second.c2.getOutput().httpResponse);
   });
   FlowStageUtils.describeRunAndCheckIsValid(second.b, () => {
+    // When CR3 is accepted and merged into the Booking Spec, this will be a 400 error, not a 409
     itShouldReturnAnOpenBookingError('IncompleteOrderItemError', 409, () => second.b.getOutput().httpResponse);
   });
 });
