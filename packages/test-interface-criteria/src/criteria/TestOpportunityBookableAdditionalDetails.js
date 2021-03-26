da77476adfe406b463ae40a6745289e2016585ee
Civ Sivakumaran <civ@imin.co>
2021-03-26 10:52:43 +0000

    add TestOpportunityBookableAdditionalDetails

---
+++ packages/test-interface-criteria/src/criteria/TestOpportunityBookableAdditionalDetails.js
@@ -0,0 +1,40 @@
+const {
+  createCriteria,
+  mustRequireAdditionalDetails,
+  remainingCapacityMustBeAtLeastTwo,
+  startDateMustBe2HrsInAdvance,
+  eventStatusMustNotBeCancelledOrPostponed,
+  mustHaveBookableOffer,
+} = require('./criteriaUtils');
+
+const TestOpportunityBookableAdditionalDetails = createCriteria({
+  name: 'TestOpportunityBookableAdditionalDetails',
+  opportunityConstraints: [
+    [
+      'Remaining capacity must be at least two (or one for IndividualFacilityUse)',
+      remainingCapacityMustBeAtLeastTwo,
+    ],
+    [
+      'Start date must be 2hrs in advance for random tests to use',
+      startDateMustBe2HrsInAdvance,
+    ],
+    [
+      'eventStatus must not be Cancelled or Postponed',
+      eventStatusMustNotBeCancelledOrPostponed,
+    ],
+  ],
+  offerConstraints: [
+    [
+      'Must have "bookable" offer',
+      mustHaveBookableOffer,
+    ],
+    [
+      'Must require additional details',
+      mustRequireAdditionalDetails,
+    ],
+  ],
+});
+
+module.exports = {
+  TestOpportunityBookableAdditionalDetails,
+};
