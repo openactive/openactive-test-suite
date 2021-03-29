130326e4ebc7fbb9802bf28cdf124b0c3291583d
Civ Sivakumaran <civ@imin.co>
2021-03-29 15:36:43 +0100

    add eslint disable

--- packages/openactive-broker-microservice/app.js
+++ packages/openactive-broker-microservice/app.js
@@ -98,6 +98,7 @@ async function validateAndStoreValidationResults(data, validator) {
     // Ignore the error that a SessionSeries must have children as they haven't been combined yet.
     // This is being done because I don't know if there is a validator.validationMode for this, and without ignoring the broker does not run
     if (!(data['@type'] === 'SessionSeries' && errorShortMessage === 'A SessionSeries must have an eventSchedule or at least one subEvent.')) {
+      // eslint-disable-next-line no-continue
       continue;
     }
 
