3f0c46a93fb70687cdc52c2e530bda9c816bcd7c
Civ Sivakumaran <civ@imin.co>
2021-03-29 15:06:36 +0100

    ignore SessionSeries validation error

--- packages/openactive-broker-microservice/app.js
+++ packages/openactive-broker-microservice/app.js
@@ -95,6 +95,12 @@ async function validateAndStoreValidationResults(data, validator) {
     const errorShortMessage = error.message.split('\n')[0];
     const errorKey = `${error.path}: ${errorShortMessage}`;
 
+    // Ignore the error that a SessionSeries must have children as they haven't been combined yet.
+    // This is being done because I don't know if there is a validator.validationMode for this, and without ignoring the broker does not run
+    if (!(data['@type'] === 'SessionSeries' && errorShortMessage === 'A SessionSeries must have an eventSchedule or at least one subEvent.')) {
+      continue;
+    }
+
     // Create a new entry if this is a new error
     let currentValidationResults = validationResults.get(errorKey);
     if (!currentValidationResults) {
