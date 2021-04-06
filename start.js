61a26386034a6c3736cf394390e79f34abf8971d
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2021-04-06 18:26:27 +0100

    Fix CI

--- start.js
+++ start.js
@@ -9,7 +9,8 @@ const prompts = require('prompts');
 
 // Override waitForHarvestCompletion in the environment
 const nodeConfig = process.env.NODE_CONFIG ? JSON.parse(process.env.NODE_CONFIG) : {};
-nodeConfig.waitForHarvestCompletion = true;
+if (!nodeConfig.broker) nodeConfig.broker = {};
+nodeConfig.broker.waitForHarvestCompletion = true;
 process.env.NODE_CONFIG = JSON.stringify(nodeConfig);
 
 let microservice = null;
@@ -30,39 +31,40 @@ microservice.on('close', (code) => {
   if (code !== 0 && code !== null) process.exitCode = code;
 });
 
-// When integration tests exit, kill the microservice
-/*integrationTests.on('close', (code) => {
-  if (microservice !== null) microservice.kill();
-  // If exit code is not successful, use this for the result of the whole process (to ensure CI fails)
-  if (code !== 0 && code !== null) process.exitCode = code;
-});
-*/
-
 function launchIntegrationTests(args) {
   integrationTests = fork('./node_modules/jest/bin/jest.js', args, { cwd: './packages/openactive-integration-tests/'} );
-  integrationTests.on('close', async (code) => {
-    const testArgs = args.join(' ');
-  
-    console.log(`
+  if (nodeConfig.ci) {
+    // When integration tests exit, kill the microservice
+    integrationTests.on('close', (code) => {
+      if (microservice !== null) microservice.kill();
+      // If exit code is not successful, use this for the result of the whole process (to ensure CI fails)
+      if (code !== 0 && code !== null) process.exitCode = code;
+    });
+  } else {
+    integrationTests.on('close', async (code) => {
+      const testArgs = args.join(' ');
+    
+      console.log(`
 When data feeds are stable or when using 'controlled' mode, tests can be rerun
 quickly without reharvesting. However, for 'random' mode, if data feeds have
 been updated without the RPDE 'modified' property being updated (e.g. when
 implementing the RPDE feed itself), please exit the test suite and rerun it,
 in order to harvest the latest data.
 `);
-
-    const response = await prompts({
-      type: 'text',
-      name: 'testArgs',
-      message: 'Rerun tests (esc to exit)?',
-      initial: testArgs
-    });
   
-    if (typeof response.testArgs === 'string') {
-      launchIntegrationTests(response.testArgs.split(' '));
-    } else {
-      // If esc, abort, ctrl+c, ctrl+d was pressed
-      if (microservice !== null) microservice.kill();
-    }
-  });
+      const response = await prompts({
+        type: 'text',
+        name: 'testArgs',
+        message: 'Rerun tests (esc to exit)?',
+        initial: testArgs
+      });
+    
+      if (typeof response.testArgs === 'string') {
+        launchIntegrationTests(response.testArgs.split(' '));
+      } else {
+        // If esc, abort, ctrl+c, ctrl+d was pressed
+        if (microservice !== null) microservice.kill();
+      }
+    });
+  }
 }
