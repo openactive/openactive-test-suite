083954973f32992f58eafb041dd08ea224258263
Luke Winship <luke.winship@gmail.com>
2021-04-16 14:03:37 +0100

    access-bardcode-seller-test

--- packages/openactive-integration-tests/test/features/access/access-pass-barcode-seller-provided/implemented/access-barcode-seller-test.js
+++ packages/openactive-integration-tests/test/features/access/access-pass-barcode-seller-provided/implemented/access-barcode-seller-test.js
@@ -1,11 +1,7 @@
-/* eslint-disable no-unused-vars */
-const chakram = require('chakram');
 const { expect } = require('chai');
 
 const { FeatureHelper } = require('../../../../helpers/feature-helper');
-const { GetMatch, C1, C2, B } = require('../../../../shared-behaviours');
-
-/* eslint-enable no-unused-vars */
+const { FlowStageRecipes, FlowStageUtils } = require('../../../../helpers/flow-stages');
 
 FeatureHelper.describeFeature(module, {
   testCategory: 'access',
@@ -19,59 +15,20 @@ FeatureHelper.describeFeature(module, {
   // The secondary opportunity criteria to use for multiple OrderItem tests
   controlOpportunityCriteria: 'TestOpportunityBookable',
 },
-function (configuration, orderItemCriteria, featureIsImplemented, logger, state, flow) {
-  beforeAll(async function () {
-    await state.fetchOpportunities(orderItemCriteria);
+function (configuration, orderItemCriteriaList, featureIsImplemented, logger) {
+  const { fetchOpportunities, c1, c2, b } = FlowStageRecipes.initialiseSimpleC1C2BFlow(orderItemCriteriaList, logger);
 
-    return chakram.wait();
-  });
-
-  afterAll(async function () {
-    await state.deleteOrder();
-    return chakram.wait();
-  });
-
-  describe('Get Opportunity Feed Items', function () {
-    (new GetMatch({
-      state, flow, logger, orderItemCriteria,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('C1', function () {
-    (new C1({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('C2', function () {
-    (new C2({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-  });
-
-  describe('B', function () {
-    (new B({
-      state, flow, logger,
-    }))
-      .beforeSetup()
-      .successChecks()
-      .validationTests();
-
+  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(fetchOpportunities);
+  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(c1);
+  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(c2);
+  FlowStageUtils.describeRunAndCheckIsSuccessfulAndValid(b, () => {
     it('Response should include Barcode accessPass with url, text and codeType field', () => {
-      expect(state.bResponse.body.orderedItem).to.be.an('array')
+      const { orderedItem } = b.getOutput().httpResponse.body;
+      expect(orderedItem).to.be.an('array')
         .that.has.lengthOf.above(0)
-        .and.has.lengthOf(orderItemCriteria.length);
+        .and.has.lengthOf(orderItemCriteriaList.length);
 
-      for (const orderItem of state.bResponse.body.orderedItem) {
+      for (const orderItem of orderedItem) {
         // Virtual sessions do not have accessPasses so need to be filtered out
         if (!orderItem.accessChannel || orderItem.accessChannel['@type'] !== 'VirtualLocation') {
           expect(orderItem.accessPass).to.be.an('array');
