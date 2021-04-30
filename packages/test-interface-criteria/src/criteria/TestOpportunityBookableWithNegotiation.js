38093ad49d0874f6ab371d1309f16dfa2a4b49d5
Luke Winship <luke.winship@gmail.com>
2021-04-30 12:12:27 +0100

    fix:  Fix Negotiation criteria & Upgrade data-models
    Previously, negotiation criteria allowed opps that require additional details, which meant that tests were occasionally failing

--- packages/test-interface-criteria/src/criteria/TestOpportunityBookableWithNegotiation.js
+++ packages/test-interface-criteria/src/criteria/TestOpportunityBookableWithNegotiation.js
@@ -5,35 +5,42 @@ const {
   startDateMustBe2HrsInAdvance,
   eventStatusMustNotBeCancelledOrPostponed,
   mustHaveBookableOffer,
+  mustNotRequireAttendeeDetails,
 } = require('./criteriaUtils');
+const { InternalTestOpportunityBookable } = require('./internal/InternalTestOpportunityBookable');
 
 const TestOpportunityBookableWithNegotiation = createCriteria({
   name: 'TestOpportunityBookableWithNegotiation',
   opportunityConstraints: [
-    [
-      'Remaining capacity must be at least two (or one for IndividualFacilityUse)',
-      remainingCapacityMustBeAtLeastTwo,
-    ],
-    [
-      'Start date must be 2hrs in advance for random tests to use',
-      startDateMustBe2HrsInAdvance,
-    ],
-    [
-      'eventStatus must not be Cancelled or Postponed',
-      eventStatusMustNotBeCancelledOrPostponed,
-    ],
+    // [
+    //   'Remaining capacity must be at least two (or one for IndividualFacilityUse)',
+    //   remainingCapacityMustBeAtLeastTwo,
+    // ],
+    // [
+    //   'Start date must be 2hrs in advance for random tests to use',
+    //   startDateMustBe2HrsInAdvance,
+    // ],
+    // [
+    //   'eventStatus must not be Cancelled or Postponed',
+    //   eventStatusMustNotBeCancelledOrPostponed,
+    // ],
   ],
   offerConstraints: [
-    [
-      'Must have "bookable" offer',
-      mustHaveBookableOffer,
-    ],
+    // [
+    //   'Must have "bookable" offer',
+    //   mustHaveBookableOffer,
+    // ],
     [
       'Must allow proposal amendment',
       mustAllowProposalAmendment,
     ],
+    // [
+    //   'Must not require attendee details',
+    //   mustNotRequireAttendeeDetails,
+    // ],
   ],
   testDataShape: () => ({}), // TODO: Add data shape
+  includeConstraintsFromCriteria: InternalTestOpportunityBookable,
 });
 
 module.exports = {
