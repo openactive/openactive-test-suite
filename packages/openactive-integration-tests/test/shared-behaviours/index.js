f9ce19b0c99999ed6578ba024ae213c006cb6b83
Luke Winship <luke.winship@gmail.com>
2021-05-04 17:22:58 +0100

    remove GetMatch

--- packages/openactive-integration-tests/test/shared-behaviours/index.js
+++ packages/openactive-integration-tests/test/shared-behaviours/index.js
@@ -1,12 +1,10 @@
 const { GetDatasetSite } = require('./get-dataset-site');
-const { GetMatch } = require('./get-match');
 const { TestInterfaceAction } = require('./test-interface-action');
 const { Common } = require('./common');
 const { OpenIDConnectFlow } = require('./open-id-connect-flow');
 
 module.exports = {
   GetDatasetSite,
-  GetMatch,
   TestInterfaceAction,
   Common,
   OpenIDConnectFlow,
