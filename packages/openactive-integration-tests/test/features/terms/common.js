51faa5db7c08b0fa311634882ce88fc02cd22781
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2021-06-28 15:45:39 +0100

    Minor fixes for terms of service and logging

--- packages/openactive-integration-tests/test/features/terms/common.js
+++ packages/openactive-integration-tests/test/features/terms/common.js
@@ -11,9 +11,11 @@ function expectTermsOfServiceToExistAndBeValid(termsOfService) {
   expect(termsOfService).to.be.an('array')
     .that.has.lengthOf.at.least(1);
   for (const termsOfServiceItem of /** @type {unknown[]} */(termsOfService)) {
+    // The validator will ensure that the correct types are used in this array,
+    // and that the correct required properties exist.
+    // Hence we only need to sanity check for the existence of a single property.
     // @ts-expect-error chai-url doesn't have a types package
     expect(termsOfServiceItem).to.have.property('url').that.has.protocol('https');
-    expect(termsOfServiceItem).to.have.property('@type', 'PrivacyPolicy');
   }
 }
 
