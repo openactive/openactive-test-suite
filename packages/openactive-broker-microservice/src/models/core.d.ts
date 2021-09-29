1a4efaef94b44471940ced5200b357ba3e10a396
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2021-09-29 09:13:44 +0100

    feat: Additional filters and flexibility for auth testing (#452)

--- packages/openactive-broker-microservice/src/models/core.d.ts
+++ packages/openactive-broker-microservice/src/models/core.d.ts
@@ -5,10 +5,12 @@ export type BookingPartnerIdentifier = 'primary' | 'secondary';
 export type FeedContext = {
   currentPage: string;
   pages: number;
+  sleepPages: number;
   items: number;
   responseTimes: number[];
   totalItemsQueuedForValidation: number;
   validatedItems: number;
   sleepMode?: boolean;
+  isInitialHarvestComplete?: boolean;
   timeToHarvestCompletion?: string;
 };
