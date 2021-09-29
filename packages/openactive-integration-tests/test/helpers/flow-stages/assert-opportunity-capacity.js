d8d6f21e154d346cad1ab7336863c3b8ba9e2915
Luke Winship <luke.winship@gmail.com>
2021-09-29 09:37:09 +0100

    fix for multiple

--- packages/openactive-integration-tests/test/helpers/flow-stages/assert-opportunity-capacity.js
+++ packages/openactive-integration-tests/test/helpers/flow-stages/assert-opportunity-capacity.js
@@ -1,7 +1,6 @@
 const { utils: { getRemainingCapacity } } = require('@openactive/test-interface-criteria');
 const { assertIsNotNullish } = require('@tool-belt/type-predicates');
-const { expect } = require('chai');
-const { zip } = require('lodash');
+const { uniqBy } = require('lodash');
 const { FlowStage } = require('./flow-stage');
 const { itSuccessChecksOpportunityFeedUpdateCollector } = require('./opportunity-feed-update');
 
@@ -14,10 +13,6 @@ const { itSuccessChecksOpportunityFeedUpdateCollector } = require('./opportunity
  * @typedef {import('./flow-stage').FlowStageOutput} FlowStageOutput
  */
 
-//  * @typedef {Required<Pick<FlowStageOutput, 'orderItems'>>} Input
-//  * @typedef {{ opportunity: OrderItem['orderedItem'], orderItems: OrderItem[] }} ArgsToGetExpectedCapacity
-//  * @typedef {(opportunity: OrderItem['orderedItem'], orderItems: OrderItem[]) => number} GetOpportunityExpectedCapacity
-
 /**
  * @typedef {Required<Pick<FlowStageOutput, 'opportunityFeedExtractResponses' | 'orderItems'>>} Input
  * @typedef {Required<Pick<FlowStageOutput, 'opportunityFeedExtractResponses'>>} Output
@@ -26,84 +21,64 @@ const { itSuccessChecksOpportunityFeedUpdateCollector } = require('./opportunity
  * @typedef {(opportunity: OrderItem['orderedItem'], count: number) => number} GetOpportunityExpectedCapacity
  */
 
+// TODO TODO TODO doc and better name
 /**
  * @param {GetOpportunityExpectedCapacity} getOpportunityExpectedCapacity
  * @param {ChakramResponse[]} opportunityFeedExtractResponses
- * @returns Opportunity ID -> expected capacity for that Opportunity
+ * @param {OrderItem[]} orderItems
+ * @returns Opportunity ID -> { ... }
  */
-function getExpectedCapacitiesPerOpportunityId(getOpportunityExpectedCapacity, opportunityFeedExtractResponses) {
-  // First, get the list of distinct Opportunities, along with the list of OrderItems that goes with each
-  /** @type {Map<string, ArgsToGetExpectedCapacity>} */
-  const argsToGetExpectedCapacitiesPerOpportunityId = new Map();
-  for (const opportunityFeedExtractResponse of opportunityFeedExtractResponses) {
-    const opportunity = getAndAssertOpportunityFromOpportunityFeedExtractResponse(opportunityFeedExtractResponse);
-    const opportunityId = opportunity['@id'];
-    const existing = argsToGetExpectedCapacitiesPerOpportunityId.get(opportunityId);
-    if (existing) {
-      existing.count += 1;
-      // existing.orderItems.push(opportunityFeedExtractResponse);
-    } else {
-      /** @type {ArgsToGetExpectedCapacity} */
-      const args = {
-        opportunity,
-        count: 1,
-        // orderItems: [opportunityFeedExtractResponse],
-      };
-      argsToGetExpectedCapacitiesPerOpportunityId.set(opportunityId, args);
-    }
-  }
-  // Then, get expected capacities using getOpportunityExpectedCapacity(..)
-  /** @type {Map<string, number>} */
-  const expectedCapacitiesPerOpportunityId = new Map();
-  for (const [opportunityId, args] of argsToGetExpectedCapacitiesPerOpportunityId) {
-    const expectedCapacity = getOpportunityExpectedCapacity(args.opportunity, args.count);
-    expectedCapacitiesPerOpportunityId.set(opportunityId, expectedCapacity);
-  }
-  return expectedCapacitiesPerOpportunityId;
+function getExpectedCapacityForEachUniqueOpportunity(getOpportunityExpectedCapacity, opportunityFeedExtractResponses, orderItems) {
+  const uniqueOpportunities = uniqBy(opportunityFeedExtractResponses,
+    response => getAndAssertOpportunityFromOpportunityFeedExtractResponse(response)['@id'])
+    .map(response => getAndAssertOpportunityFromOpportunityFeedExtractResponse(response));
+  return new Map(uniqueOpportunities.map((opportunity) => {
+    const count = opportunityFeedExtractResponses.filter(response => (
+      getAndAssertOpportunityFromOpportunityFeedExtractResponse(response)['@id'] === opportunity['@id'])).length;
+    const orderItem = orderItems.find(item => item.orderedItem['@id'] === opportunity['@id']);
+    assertIsNotNullish(orderItem);
+    const expectedCapacity = getOpportunityExpectedCapacity(opportunity, count);
+    return [opportunity['@id'], {
+      opportunity,
+      orderItem,
+      expectedCapacity,
+    }];
+  }));
 }
-
-const DO_RUN_OPPORTUNITY_FEED_EXTRACT_ORDER_ITEM_PARITY_ASSERTIONS = true;
 
 /**
  * @param {object} args
  * @param {GetOpportunityExpectedCapacity} args.getOpportunityExpectedCapacity
- * @param {FlowStageOutput['orderItems']} args.orderItems
  * @param {ChakramResponse[]} args.opportunityFeedExtractResponses
+ * @param {FlowStageOutput['orderItems']} args.orderItems
  * @param {RequestHelperType} args.requestHelper
  * @returns {Promise<Output>}
  */
 async function runAssertOpportunityCapacity({
   getOpportunityExpectedCapacity,
-  orderItems,
   opportunityFeedExtractResponses: initialOpportunityFeedExtractResponses,
+  orderItems,
   requestHelper,
 }) {
-  const expectedCapacitiesPerOpportunityId = getExpectedCapacitiesPerOpportunityId(getOpportunityExpectedCapacity, initialOpportunityFeedExtractResponses);
-  const zippedWithOrderItems = zip(initialOpportunityFeedExtractResponses, orderItems);
-  // PRE-CONDITION: opportunityFeedExtractResponses and orderItems are matched up so that they can be zipped together
-  if (DO_RUN_OPPORTUNITY_FEED_EXTRACT_ORDER_ITEM_PARITY_ASSERTIONS) {
-    expect(orderItems).to.have.lengthOf(initialOpportunityFeedExtractResponses.length);
-    for (const [opportunityFeedExtractResponse, orderItem] of zippedWithOrderItems) {
-      const opportunity = getAndAssertOpportunityFromOpportunityFeedExtractResponse(opportunityFeedExtractResponse);
-      expect(orderItem.orderedItem['@id']).to.equal(opportunity['@id']);
+  const expectedCapacityForEachUniqueOpportunity = getExpectedCapacityForEachUniqueOpportunity(
+    getOpportunityExpectedCapacity, initialOpportunityFeedExtractResponses, orderItems,
+  );
+  // TODO TODO TODO put this pattern into a function
+  /** @type {Map<string, Promise<ChakramResponse>>} */
+  const reusableGetMatchPromises = new Map();
+  const opportunityFeedExtractResponsePromises = initialOpportunityFeedExtractResponses.map((opportunityFeedExtractResponse) => {
+    const opportunityId = getAndAssertOpportunityFromOpportunityFeedExtractResponse(opportunityFeedExtractResponse)['@id'];
+    if (reusableGetMatchPromises.has(opportunityId)) {
+      return reusableGetMatchPromises.get(opportunityId);
     }
-  }
-  /* TODO could optimise to not have to duplicate getMatch requests for OrderItems that represent the same Opportunity
-  - like in fetchOpportunityFeedExtractResponses(..) although it seems that that function could be improved to optimize
-  more consistently */
-  const opportunityFeedExtractResponses = await Promise.all(zippedWithOrderItems.map(async ([opportunityFeedExtractResponse, orderItem]) => {
-    const opportunity = getAndAssertOpportunityFromOpportunityFeedExtractResponse(opportunityFeedExtractResponse);
-    const opportunityId = opportunity['@id'];
-    const expectedCapacity = expectedCapacitiesPerOpportunityId.get(opportunityId);
-    assertIsNotNullish(expectedCapacity);
-    return await requestHelper.getMatch(opportunityId, orderItem.position, true, { expectedCapacity });
-  }));
-  // const result = await Promise.all(orderItems.map(async (orderItem) => {
-  //   const opportunityId = orderItem.orderedItem['@id'];
-  //   const expectedCapacity = expectedCapacitiesPerOpportunityId.get(opportunityId);
-  //   assertIsNotNullish(expectedCapacity);
-  //   return await requestHelper.getMatch(orderItem.orderedItem['@id'], orderItem.position, true, { expectedCapacity });
-  // }));
+    const getMatchDetails = expectedCapacityForEachUniqueOpportunity.get(opportunityId);
+    assertIsNotNullish(getMatchDetails);
+    const { opportunity, orderItem, expectedCapacity } = getMatchDetails;
+    const matchPromise = requestHelper.getMatch(opportunity['@id'], orderItem.position, true, { expectedCapacity });
+    reusableGetMatchPromises.set(opportunityId, matchPromise);
+    return matchPromise;
+  });
+  const opportunityFeedExtractResponses = await Promise.all(opportunityFeedExtractResponsePromises);
   return {
     opportunityFeedExtractResponses,
   };
