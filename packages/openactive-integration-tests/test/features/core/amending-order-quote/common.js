de40733ea88cc2ef7079da60023ebda6486d164c
Luke Winship <luke.winship@gmail.com>
2021-10-07 18:14:52 +0100

    skip capacity asserts after amendment

--- packages/openactive-integration-tests/test/features/core/amending-order-quote/common.js
+++ packages/openactive-integration-tests/test/features/core/amending-order-quote/common.js
@@ -2,6 +2,8 @@ const { expect } = require('chai');
 const { AssertOpportunityCapacityFlowStage } = require('../../../helpers/flow-stages/assert-opportunity-capacity');
 const { Common } = require('../../../shared-behaviours');
 
+const RUN_TESTS_WHICH_FAIL_REFIMPL = process.env.RUN_TESTS_WHICH_FAIL_REFIMPL === 'true';
+
 /**
  * @typedef {import('../../../types/OpportunityCriteria').OpportunityCriteria} OpportunityCriteria
  * @typedef {import('../../../helpers/flow-stages/fetch-opportunities').FetchOpportunitiesFlowStageType} FetchOpportunitiesFlowStageType
@@ -47,6 +49,8 @@ const AmendingOrderQuoteFlowStageRecipes = {
   assertFirstAttemptOpportunitiesHaveRegainedCapacity(nameOfPreviousStage, prerequisite, defaultFlowStageParams, firstAttemptFetchOpportunities) {
     return new AssertOpportunityCapacityFlowStage({
       ...defaultFlowStageParams,
+      // Capacity is not correctly restored after amendment in RefImpl: https://github.com/openactive/OpenActive.Server.NET/issues/168
+      doSkip: !RUN_TESTS_WHICH_FAIL_REFIMPL,
       prerequisite,
       nameOfPreviousStage,
       // Capacity for the first Order should have reverted to how it started
