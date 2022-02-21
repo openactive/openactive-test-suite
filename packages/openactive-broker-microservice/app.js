474d51cc5f286fa73fbb45603ceae2bd5c9e123a
Luke Winship <luke.winship@gmail.com>
2022-02-21 14:46:25 +0000

    increase chunk size - 10 -> 500

--- packages/openactive-broker-microservice/app.js
+++ packages/openactive-broker-microservice/app.js
@@ -77,7 +77,24 @@ const { OrderUuidTracking } = require('./src/order-uuid-tracking/order-uuid-trac
 const { error400IfExpressParamsAreMissing } = require('./src/util/api-utils');
 const { ValidatorWorkerPool } = require('./src/validatorWorkerPool');
 
-const VALIDATOR_ITEMS_CHUNK_LENGTH = 10;
+/**
+ * A chunk length of 500 gives a memory usage by Validator Workers of roughly up to 4kb * 500 * <NUM CORES>.
+ *
+ * - A rough average size of medium-size (e.g. SessionSeries rather than ScheduledSession) OpenActive items
+ *   (uncompressed) is about 2-4kb.
+ * - So, with 8 cores, the memory usage will be roughly up to 4kb * 500 * 8 = 16MB, which is acceptable.
+ * - To clarify, this is the memory usage of just holding this data in memory. This is not counting the additional
+ *   memory usage required by validator's mechanisms.
+ *
+ * The flipside is that this results in a number of validator-input files created that is roughly:
+ *
+ * - <NUM UPDATED ITEMS IN FEEDS> / 500
+ * - This would happen if validator took so long to run that all the input files accumulated before it had run even
+ *   once.
+ * - So, with 1M items (as a very high example!) in the feeds, this could result in 2,000 files. This also seems
+ *   acceptably unlikely to cause issues (e.g. inode usage issues).
+ */
+const VALIDATOR_ITEMS_CHUNK_LENGTH = 500;
 
 let validatorInputFilenameSequenceNum = 0;
 
