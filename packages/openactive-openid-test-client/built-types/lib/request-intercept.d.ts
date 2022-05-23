aac39af0d02ac4a6a36b0e6596df3ccaf8f9976a
Samuel Scully <sbscully@gmail.com>
2022-05-23 16:43:02 +0100

    fix: Update puppeteer to avoid test runner hanging
    With recent versions of Chrome installed the puppeteer would hang.
    
    This required also installing a newer version of TypeScript to avoid
    type errors.

--- packages/openactive-openid-test-client/built-types/lib/request-intercept.d.ts
+++ packages/openactive-openid-test-client/built-types/lib/request-intercept.d.ts
@@ -28,18 +28,7 @@ export type Entry = {
  * @param {() => TActionFnResult} actionFn
  * @returns {Promise<TActionFnResult>}
  */
-export function recordWithIntercept<TActionFnResult>(recordLogEntry: (Entry: any) => {
-    type: 'request';
-    stage: string;
-    request: {
-        [k: string]: unknown;
-    };
-    response?: {
-        [k: string]: unknown;
-    };
-    isPending: boolean;
-    duration: number;
-}, stage: string, actionFn: () => TActionFnResult): Promise<TActionFnResult>;
+export function recordWithIntercept<TActionFnResult>(recordLogEntry: (Entry: any) => Entry, stage: string, actionFn: () => TActionFnResult): Promise<TActionFnResult>;
 /**
  * Intecept and output to the console all http requests made during the execution of actionFn
  * This is used primarily for the CLI
