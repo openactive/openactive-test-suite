c2adfc8a4be739a7c9be75eb1680b0350d81bc31
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2022-06-13 09:58:05 +0100

    fix: @context for IndividualFacilityUse

--- packages/openactive-broker-microservice/app.js
+++ packages/openactive-broker-microservice/app.js
@@ -1133,6 +1133,7 @@ function invertFacilityUseItems(items) {
         id: individualFacilityUse['@id'],
         data: {
           ...individualFacilityUse,
+          '@context': facilityUseItem.data['@context'],
           aggregateFacilityUse: omit(facilityUseItem.data, ['individualFacilityUse', '@context']),
         },
       });
