a910cb8468f90037eb8b62882660faf5a4dd8962
nickevansuk <2616208+nickevansuk@users.noreply.github.com>
2023-08-01 19:14:07 +0100

    Better brackets

--- packages/openactive-integration-tests/test/certification/certification-writer.js
+++ packages/openactive-integration-tests/test/certification/certification-writer.js
@@ -61,11 +61,11 @@ class CertificationWriter {
 
   get awardedTo() {
     if (this.datasetJson) {
-      return this.datasetJson.bookingService?.name ? this.datasetJson.bookingService : this.datasetJson.publisher && {
+      return this.datasetJson.bookingService?.name ? this.datasetJson.bookingService : (this.datasetJson.publisher && {
         '@type': 'Organization',
         name: this.datasetJson.publisher.name,
         url: this.datasetJson.publisher.url,
-      };
+      });
     }
     return {};
   }
