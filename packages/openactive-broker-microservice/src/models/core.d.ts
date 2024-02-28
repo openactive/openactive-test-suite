03a3e1e07d19c822ead139e95a980c2dc0793ec2
civsiv <civ@imin.co>
2024-02-28 11:07:55 +0000

    feat: abstract harvestRPDE (#642)
    * feat: abstract harvestRPDE
    
    * update dependencies
    
    * lint fixes
    
    * review changes
    
    * lint

--- packages/openactive-broker-microservice/src/models/core.d.ts
+++ packages/openactive-broker-microservice/src/models/core.d.ts
@@ -1,18 +1,3 @@
-import { SingleBar } from 'cli-progress';
-
 export type OrderFeedType = 'orders' | 'order-proposals';
 export type OrderFeedIdentifier = 'OrdersFeed' | 'OrderProposalsFeed';
 export type BookingPartnerIdentifier = string;
-
-export type FeedContext = {
-  currentPage: string;
-  pages: number;
-  items: number;
-  responseTimes: number[];
-  totalItemsQueuedForValidation: number;
-  validatedItems: number;
-  sleepMode?: boolean;
-  timeToHarvestCompletion?: string;
-
-  _progressbar?: SingleBar; // Underscore prefix hides this value from /status page
-};
