57fb593006377adbad35a4062d2c6ce813f995b3
Nick Evans <2616208+nickevansuk@users.noreply.github.com>
2024-12-20 21:49:24 +0000

    docs: Explain logging (#702)

--- packages/openactive-broker-microservice/src/util/log.js
+++ packages/openactive-broker-microservice/src/util/log.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 
 /* eslint-disable no-console */
 const logError = (x) => console.error(chalk.cyanBright(x));
-const logErrorDuringHarvest = (x) => console.error(chalk.cyanBright(`\n\n${x}\n\n\n\n\n\n\n\n\n`));
+const logErrorDuringHarvest = (x) => console.error(chalk.cyanBright(`\n\n${x}\n\n\n\n\n\n\n\n\n`)); // Above the multibar output
 const log = (x) => console.log(chalk.cyan(x));
 /* eslint-enable no-console */
 const logCharacter = (x) => process.stdout.write(chalk.cyan(x));
